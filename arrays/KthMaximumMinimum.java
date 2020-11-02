39a1a9ce7d5820e3cadd26acb3f605fa20455a0e
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-02 17:08:06 +0530

    Program for finding kth min and max in array

--- arrays/KthMaximumMinimum.java
+++ arrays/KthMaximumMinimum.java
@@ -13,3 +13,35 @@
 	
 */
 
+import java.util.*;
+public class KthMaximumMinimum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements in array");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		System.out.println("Enter the elements of array");
+		for(int i=0; i<n; i++) {
+			System.out.println("Enter element "+(i+1));
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the value of k");
+		int k = sc.nextInt();
+		sc.close();
+		Arrays.sort(arr);
+		System.out.println("Sorted array is: ");
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		
+		System.out.println("Maximum kth element in array is " +  arr[n-k]);
+		
+		System.out.println("Minimum kth element in array is " +  arr[k-1]);
+	}
+	
+}
