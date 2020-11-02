25857a69d89d67a8b2bd5eea35b5e0bfc8dcac52
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-02 16:51:33 +0530

    Program for finding min and max in array

--- arrays/MinimumMaximum.java
+++ arrays/MinimumMaximum.java
@@ -1,3 +1,36 @@
 /*
 	Find the maximum and minimum element in an array
 */
+
+import java.util.*;
+public class MaximumMinimum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements in array");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		System.out.println("Enter the elements of array");
+		for(int i=0; i<n; i++) {
+			System.out.println("Enter element "+(i+1));
+			arr[i] = sc.nextInt();
+		}
+		
+		int min = arr[0];
+		int max = arr[0];
+		
+		for(int i=0; i<n; i++)
+		{
+			if(arr[i]>max)
+				max = arr[i];
+			
+			if(arr[i]<min)
+				min = arr[i];
+		}
+		
+		System.out.println("Minimum element: "+min+"\nMaximum element: "+max);
+	}
+
+}
