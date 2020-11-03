2cc8b60b0f77cba43bac2c2978a76c568c142455
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-03 14:40:45 +0530

    Program for moving negative elements to one side

--- arrays/MoveNegativeElementToOneSide.java
+++ arrays/MoveNegativeElementToOneSide.java
@@ -6,3 +6,44 @@
 	Output: -12 -13 -5 -7 -3 -6 11 6 5
 	Note: Order of elements is not important here.
 */
+
+import java.util.*;
+public class MoveNegativeElementToOneSide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements in array");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		System.out.println("Enter the elements of array");
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		int j=0;
+		for(int i=0; i<n; i++)
+		{
+			if(arr[i]<0)
+			{
+				if(i!=j)
+				{
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				j++;
+			}
+		}
+		
+		System.out.println("Resultant array: ");
+		for(int i: arr)
+		{
+			System.out.print(i + " ");
+		}
+	}
+
+}
