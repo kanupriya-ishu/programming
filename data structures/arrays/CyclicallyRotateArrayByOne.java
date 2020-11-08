9807a8c1b8449cac24ab321998674030814ef425
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-08 21:44:11 +0530

    Program for cyclically rotating array by one

--- data structures/arrays/CyclicallyRotateArrayByOne.java
+++ data structures/arrays/CyclicallyRotateArrayByOne.java
@@ -24,3 +24,43 @@
 	5 1 2 3 4
 	3 9 8 7 6 4 2 1
 */
+
+import java.util.*;
+public class CyclicallyRotateArrayByOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of testcases");
+		int T = sc.nextInt();
+		
+		for(int i=0; i<T; i++) {
+			System.out.println("Enter number of elements in array");
+			int n = sc.nextInt();
+			
+			int[] arr = new int[n];
+			System.out.println("Enter the elements of array");
+			for(int j=0; j<n; j++)
+			{
+				arr[j] = sc.nextInt();
+			}
+			
+			int temp = arr[n-1];
+			for(int j=n-1; j>0; j--)
+			{
+				arr[j] = arr[j-1];
+			}
+			
+			arr[0] = temp;
+			
+			System.out.println("After rotation: ");
+			for(int element: arr) {
+				System.out.print(element + " ");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
