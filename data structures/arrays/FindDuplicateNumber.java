2ba8a72abafde481f504f9b5346bf9f98ce4115b
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-09 16:50:11 +0530

    Program for 'find duplicate in an array of N+1 Integers'

--- data structures/arrays/FindDuplicateNumber.java
+++ data structures/arrays/FindDuplicateNumber.java
@@ -20,3 +20,38 @@
 	Input: nums = [1,1,2]
 	Output: 1
 */
+
+import java.util.*;
+public class FindDuplicateNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number of elements");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		System.out.println("Enter the elements of array:");
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		int[] count = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			count[arr[i]-1]++;
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(count[i]>1) {
+				System.out.println("Answer: "+ (i+1));
+				break;
+			}
+		}
+	}
+
+}
