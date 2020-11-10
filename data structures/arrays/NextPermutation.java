696e34e8988b588f11f60976c7a8ec0c2c186c19
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-10 17:11:01 +0530

    Program for 'Next Permutation'

--- data structures/arrays/NextPermutation.java
+++ data structures/arrays/NextPermutation.java
@@ -30,3 +30,54 @@
 	1 <= nums.length <= 100
 	0 <= nums[i] <= 100
 */
+
+import java.util.*;
+public class NextPermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number of elements: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		System.out.println("Enter the elements of array");
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		int i = n-2;
+		while(i>=0 && arr[i]>=arr[i+1]) {
+			i--;
+		}
+		
+		if(i>=0) {
+			int j=n-1;
+			
+			while(arr[j]<=arr[i]) {
+				j--;
+			}
+			
+			swap(arr, i, j);
+		}
+		
+		reverse(arr, i+1, n-1);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	static void swap(int[] arr, int i, int j) {
+	    int temp = arr[i];
+	    arr[i] = arr[j];
+	    arr[j] = temp;
+	}
+
+	static void reverse(int[] arr, int i, int j) {
+	    while(i < j) swap(arr, i++, j--);
+	}
+
+}
