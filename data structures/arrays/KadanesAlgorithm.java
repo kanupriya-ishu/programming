bc89ca079d9bfddcc9be3624c50ab5d50c658c74
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-10 17:03:16 +0530

    Program for 'Kadane's Algorithm'

--- data structures/arrays/KadanesAlgorithm.java
+++ data structures/arrays/KadanesAlgorithm.java
@@ -29,3 +29,35 @@
 	-107 ≤ A[i] <= 107
 */
 
+import java.util.*;
+public class KadanesAlgorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number of elements in array: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		System.out.println("Enter the elements of array");
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		int maxTillNow = arr[0];
+		int maxTillEnd = arr[0];
+		
+		for(int i=1; i<n; i++) {
+			maxTillEnd = Math.max(arr[i], maxTillEnd+arr[i]);
+			maxTillNow = Math.max(maxTillEnd, maxTillNow);
+		}
+		
+		System.out.println("Answer: "+maxTillNow);
+		
+	}
+
+}
