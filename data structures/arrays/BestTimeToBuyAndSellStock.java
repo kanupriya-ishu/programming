4c29eb096fe6763ec2270c4c892100c52241fa0f
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-13 16:14:22 +0530

    Program for 'Best time to buy and Sell stock'

--- data structures/arrays/BestTimeToBuyAndSellStock.java
+++ data structures/arrays/BestTimeToBuyAndSellStock.java
@@ -18,3 +18,56 @@
 	Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
+import java.util.*;
+public class BestTimeToBuyAndSellStock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number of elements");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.println("Enter the elements of array");
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		/* // brute force method
+			int max = 0;
+			
+			for(int i=n-1; i>0; i--) {
+				
+				for(int j=0; j<i; j++) {
+					int temp = arr[i] - arr[j];
+					
+					if (temp>max) {
+						max = temp;
+					}
+				}
+			}
+		*/
+		
+		int min = arr[0];
+		int max_profit = 0;
+		int curr_profit = 0;
+		
+		for(int i=0; i<n; i++) {
+			if(arr[i]<min) {
+				min = arr[i];
+			}
+			
+			curr_profit = arr[i] - min;
+			
+			if(curr_profit>max_profit) {
+				max_profit = curr_profit;
+			}
+		}
+		
+		System.out.println("Answer: "+max_profit);
+	}
+
+}
