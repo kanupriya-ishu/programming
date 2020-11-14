d7f4f133699b3fd66abccdd52767a9b5da8cb60c
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-14 15:41:00 +0530

    Program for 'find common elements In 3 sorted arrays'

--- data structures/arrays/CommonElements.java
+++ data structures/arrays/CommonElements.java
@@ -28,3 +28,69 @@
 	1 <= n1, n2, n3 <= 10^5
 	The array elements can be both positive or negative integers.
 */
+
+import java.util.*; 
+public class CommonElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number of elements of array 1:");
+		int n1 = sc.nextInt();
+		
+		int[] arr1 = new int[n1];
+		System.out.println("Enter the elements of array 1:");
+		for(int i=0; i<n1; i++) {
+			arr1[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the number of elements of array 2:");
+		int n2 = sc.nextInt();
+		
+		int[] arr2 = new int[n2];
+		System.out.println("Enter the elements of array 2:");
+		for(int i=0; i<n2; i++) {
+			arr2[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the number of elements of array 3:");
+		int n3 = sc.nextInt();
+		
+		int[] arr3 = new int[n3];
+		System.out.println("Enter the elements of array 3:");
+		for(int i=0; i<n3; i++) {
+			arr3[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		int i=0, j=0, k=0;
+		boolean flag = false;
+		while(i<n1 && j<n2 && k<n3) {
+			if (arr1[i]==arr2[j] && arr2[j]==arr3[k]) {
+				System.out.println(arr1[i] + " ");
+				flag = true;
+				i++;
+				j++;
+				k++;
+			}
+			
+			else if(arr1[i]<arr2[j]) {
+				i++;
+			}
+			
+			else if(arr2[j]<arr3[k]) {
+				j++;
+			}
+			else {
+				k++;
+			}
+		}
+		
+		if(flag==false) {
+			System.out.println(-1);
+		}
+	}
+
+}
