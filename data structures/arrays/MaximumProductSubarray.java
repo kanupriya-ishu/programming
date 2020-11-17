d146e1a1b98cd84dd57a4a61a56b70eea82e5c7f
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-17 19:15:17 +0530

    Program for 'find maximum product subarray '

--- data structures/arrays/MaximumProductSubarray.java
+++ data structures/arrays/MaximumProductSubarray.java
@@ -28,3 +28,44 @@
 	1 <= N <= 500
 	-102 <= Arri <= 102
 */
+
+import java.util.*;
+public class MaximumProductSubarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number of elements: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.println("Enter the elements of array: ");
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+
+		int max = arr[0];
+		int min = arr[0];
+		
+		int max_product = arr[0];
+		
+		for(int i=1; i<n; i++) {
+			if(arr[i]<0) {
+				int temp = max;
+				max = min;
+				min = temp;
+			}
+			
+			max = Math.max(arr[i], max*arr[i]);
+			min = Math.min(min*arr[i], arr[i]);
+			max_product = Math.max(max, max_product);
+		}
+		
+		System.out.println( "Maximum Subarray product is "
+                + max_product);
+	}
+
+}
