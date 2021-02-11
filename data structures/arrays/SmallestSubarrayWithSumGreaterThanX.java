1c2318c21a3a55849fab6104a9922165bc93d09d
kanupriya-ishu <kanupriya2512@gmail.com>
2021-02-11 19:23:26 +0530

    Added question

--- data structures/arrays/SmallestSubarrayWithSumGreaterThanX.java
+++ data structures/arrays/SmallestSubarrayWithSumGreaterThanX.java
@@ -1,5 +1,7 @@
 /*
+Given an array of integers (A[])  and a number x, find the smallest subarray with sum greater than the given value.
 
+Note: The answer always exists. It is guaranteed that x doesn't exceed the summation of a[i] (from 1 to N).
 */
 public class SmallestSubarrayWithSumGraterThanX {
 
