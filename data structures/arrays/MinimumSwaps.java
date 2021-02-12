4d2c56dbae93c82e694ce3d93a418c01a7a74aa4
kanupriya-ishu <kanupriya2512@gmail.com>
2021-02-12 21:28:43 +0530

    Update MinimumSwaps.java

--- data structures/arrays/MinimumSwaps.java
+++ data structures/arrays/MinimumSwaps.java
@@ -14,3 +14,42 @@ Example 2:
 Input : arr[ ] = {2, 7, 9, 5, 8, 7, 4} and K = 6 
 Output :  2 
 */
+
+import java.util.*;
+public class MinimumSwaps {
+
+    public static void main(String args[]) 
+    {  
+    	int[] arr = {2, 7, 9, 5, 8, 7, 4};
+    	
+    	int n = arr.length;
+    	int k = 5;
+    	int count = 0;
+    	for(int i=0; i<n; i++)
+    	{
+    		if(arr[i]<=k)
+    			count++;
+    	}
+    	
+    	int bad = 0;
+    	for(int i=0; i<count; i++) {
+    		if(arr[i]>k)
+    			bad++;
+    	}
+    	
+    	int ans = bad;
+    	for(int i=0, j=count; j<n ; ++i,++j) {
+    		
+    		if(arr[i]>k)
+    			--bad;
+    		
+    		if(arr[j]>k)
+    			++bad;
+    		
+    		ans = Math.min(ans, bad);
+    	}
+    	
+    	System.out.println(ans);
+    }
+    
+}
