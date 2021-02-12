4de96f573dd2f15b37d84179e0fe4483ec14357c
kanupriya-ishu <kanupriya2512@gmail.com>
2021-02-12 21:27:47 +0530

    Added question

---
+++ data structures/arrays/MinimumSwaps.java
@@ -0,0 +1,16 @@
+/*
+Given an array of n positive integers and a number k. Find the minimum number of swaps required to bring all the numbers less than or equal to k together.
+
+Example 1:
+
+Input : arr[ ] = {2, 1, 5, 6, 3} and K = 3
+Output : 1
+Explanation:
+To bring elements 2, 1, 3 together, swap element '5' with '3'
+such that final array will be- arr[] = {2, 1, 3, 6, 5}
+
+Example 2:
+
+Input : arr[ ] = {2, 7, 9, 5, 8, 7, 4} and K = 6 
+Output :  2 
+*/
