980acdb884fa754c6e2561b021eca9d847e30f1a
kanupriya-ishu <kanupriya2512@gmail.com>
2021-03-31 09:51:36 +0530

    Update TrappingRainWater.java

--- data structures/arrays/TrappingRainWater.java
+++ data structures/arrays/TrappingRainWater.java
@@ -35,3 +35,45 @@ Explanation:
 No water will be trapped.
 */
 
+import java.util.*;
+public class TrappingRainWater {
+
+    public static void main(String args[]) 
+    {  
+    	int[] arr = {6,9,9};
+    	
+    	int N = arr.length;
+    	
+    	int sum = 0;
+    	
+    	// array to store maximum value on left side of the element
+    	int[] left = new int[N];
+    	
+    	// array to store maximum value on left side of the element
+    	int[] right = new int[N];
+    	
+    	left[0] = arr[0];
+    	for(int i=1; i<N; i++) {
+    		left[i] = Math.max(left[i-1], arr[i]);
+    	}
+    	
+    	right[N-1] = arr[N-1];
+    	for(int i=N-2; i>=0; i--) {
+    		right[i] = Math.max(right[i+1], arr[i]);
+    	}
+    	
+    	// accumulated water on ith bar will be difference between (minimum of left and right highest bars) and height of the bar
+    	// total accumulated water will be sum of accumulated water on every bar
+    	for(int i=0; i<N; i++) {
+    		sum += Math.min(left[i], right[i]) - arr[i];
+    	}
+    	
+    	System.out.println(sum);
+    	
+    	
+    	
+    	
+    	
+    }
+    
+}
