68294efbc2461a1531301951163a374ba39db2fe
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-02 21:41:13 +0530

    Update RowWithMax1s.java

--- data structures/matrix/RowWithMax1s.java
+++ data structures/matrix/RowWithMax1s.java
@@ -24,3 +24,52 @@ Explanation: Row 1 contains 2 1's (0-based
 indexing).
 */
 
+public class RowWithMax1s {
+
+    public static void main(String args[]) 
+    {  
+    	int arr[][] = { { 0, 0, 0, 1 },
+                { 0, 1, 1, 1 },
+                { 1, 1, 1, 1 },
+                { 0, 0, 0, 0 } };
+    	
+    	int rows = arr.length;
+    	int cols = arr[0].length;
+    	
+    	int max=0, max_row=0;
+    	int index;
+    	for(int i=0; i<rows; i++) {
+    		index = first(arr[i],0,cols-1);
+    		
+    		if(index!=-1 && cols-index>max) {
+    			max = cols-index;
+    			max_row = i;
+    		}
+    	}
+    	
+    	System.out.println("Answer: "+max_row);
+    }
+
+	public static int first(int[] sub, int start, int end) {
+		if(start<=end) {
+			int mid = start + (end-start)/2;
+			
+			if((mid==0 || sub[mid-1]==0) && sub[mid]==1)
+				return mid;
+			
+			else if(sub[mid]==0) {
+				return first(sub, mid+1, end);
+			}
+			
+			else {
+				return first(sub, start, mid-1);
+			}
+			
+		}
+		
+		return -1;
+	}
+    
+    
+    
+}
