5f7dd5b22327a02e54b5963098289982389f733c
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-02 22:07:50 +0530

    Update RotateBy90.java

--- data structures/matrix/RotateBy90.java
+++ data structures/matrix/RotateBy90.java
@@ -20,3 +20,24 @@ Output:
 3 1
 4 2 
 */
+
+public class RotateBy90 {
+
+    public static void main(String args[]) 
+    {  
+    	int arr[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+    	int rows = arr.length;
+    	int cols = arr[0].length;
+    	
+    	for(int j=0; j<cols; j++) {
+    		for(int i=rows-1; i>=0; i--) {
+    			System.out.print(arr[i][j]+" ");
+    		}
+    		System.out.println();
+    	}
+    }
+   
+}
