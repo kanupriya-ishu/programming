ff4e67e3912adfae9e993f6e2b3bfdad78cf1008
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-02 22:00:25 +0530

    Added Question

---
+++ data structures/matrix/RotateBy90.java
@@ -0,0 +1,22 @@
+/*
+Question:
+Given a square matrix, turn it by 90 degrees in clockwise direction without using any extra space.
+
+Examples: 
+
+Input:
+1 2 3 
+4 5 6
+7 8 9  
+Output:
+7 4 1 
+8 5 2
+9 6 3
+
+Input:
+1 2
+3 4
+Output:
+3 1
+4 2 
+*/
