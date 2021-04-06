1200de5ae864827433c29506cb7a3835ae4a4300
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-06 15:37:27 +0530

    Update ReverseString.java

--- data structures/string/ReverseString.java
+++ data structures/string/ReverseString.java
@@ -14,3 +14,36 @@ Input: s = ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
 */
 
+import java.util.*;
+public class Test {
+
+    public static void main(String args[]) 
+    {  
+    	String s = "hello world";
+    	char[] str = s.toCharArray();
+    	int front = 0;
+        int rear = s.length() - 1;
+        
+        while(front < rear)
+        {
+            swap(str, front, rear);
+            front++;
+            rear--;
+        }
+        
+        String ans = "";
+        
+        for(int i=0; i<str.length; i++) {
+        	ans += str[i];
+        }
+        System.out.println(ans);
+        
+    }
+    
+    public static void swap(char[] s, int front, int rear)
+    {
+        char temp = s[front];
+        s[front] = s[rear];
+        s[rear] = temp;
+    }
+}
