f5d4d822e6ab0b161f2391e95bea2ebaadffc25d
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-09 22:39:49 +0530

    Update AllSubsequences.java

--- data structures/string/AllSubsequences.java
+++ data structures/string/AllSubsequences.java
@@ -9,3 +9,28 @@ Output : a, b, c, ab, bc, ac, abc
 Input : aaa
 Output : a, aa, aaa
 */
+
+import java.util.*;
+public class AllSubsequences {
+	
+	static List<String> l = new ArrayList<>();
+	
+    public static void main(String args[]) 
+    {  
+    	String s = "abcd";
+    	findSubSequence(s, "");
+    	System.out.println(l);
+    }
+
+	public static void findSubSequence(String s1, String s2) {
+		
+		if(s1.length()==0) {
+			l.add(s2);
+			return;
+		}
+		
+		findSubSequence(s1.substring(1), s2+s1.charAt(0));
+		
+		findSubSequence(s1.substring(1), s2); 
+	} 
+}
