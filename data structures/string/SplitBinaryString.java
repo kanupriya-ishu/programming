ae441e1828765e91683cf2e812856e8f06c648e1
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-09 23:45:17 +0530

    Update SplitBinaryString.java

--- data structures/string/SplitBinaryString.java
+++ data structures/string/SplitBinaryString.java
@@ -11,3 +11,34 @@ The required substrings are “01”, “0011”, “01” and “01”.
 Input: str = “0111100010”
 Output: 3
 */
+
+import java.util.*;
+public class SplitBinaryString {
+	
+    public static void main(String args[]) 
+    {  
+    	String s = "0100110101";
+    	System.out.println(countBinaryString(s));
+    }
+
+
+	private static int countBinaryString(String s) {
+		int n = s.length();
+		int count0 = 0, count1 = 0, cnt = 0;
+		
+		for(int i=0; i<n; i++) {
+			if(s.charAt(i)=='0')
+				count0++;
+			else
+				count1++;
+			
+			if(count0 == count1)
+				cnt++;
+		}
+		
+		if(count0 != count1)
+			return -1;
+		
+		return cnt;
+	}
+}
