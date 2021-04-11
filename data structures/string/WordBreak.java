33720d7cb0852257e88f96c70b777f96debeaeff
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-11 16:28:34 +0530

    Update WordBreak.java

--- data structures/string/WordBreak.java
+++ data structures/string/WordBreak.java
@@ -16,3 +16,44 @@ Output: Yes
 The string can be segmented as "i like samsung" 
 or "i like sam sung".
 */
+
+import java.util.*;
+public class WordBreak {
+	
+	private static Set<String> set = new HashSet<>();
+	
+    public static void main(String args[]) 
+    {  
+    	String temp_dictionary[] = {"mobile","samsung","sam","sung",
+                "man","mango","icecream","and",
+                "go","i","like","ice","cream"};
+    	
+    	for(String i: temp_dictionary) {
+    		set.add(i);
+    	}
+    	
+    	System.out.println(wordBreak("ilikesamsung"));
+        System.out.println(wordBreak("iiiiiiii"));
+        System.out.println(wordBreak(""));
+        System.out.println(wordBreak("ilikelikeimangoiii"));
+        System.out.println(wordBreak("samsungandmango"));
+        System.out.println(wordBreak("samsungandmangok"));
+    	
+    }
+
+	private static boolean wordBreak(String str) {
+		int n = str.length();
+		if(n==0)
+			return true;
+		
+		for(int i=0; i<=n; i++) {
+			if(set.contains(str.substring(0, i)) && wordBreak(str.substring(i,n))) {
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
+    
+}
