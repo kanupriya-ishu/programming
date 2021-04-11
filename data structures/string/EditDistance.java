81d72c578bc73b69554b30c0e66807eda6aac912
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-11 12:29:22 +0530

    Added question

---
+++ data structures/string/EditDistance.java
@@ -0,0 +1,24 @@
+/*
+Question:
+Given two strings s and t. Find the minimum number of operations that need to be performed on str1 to convert it to str2. The possible operations are:
+
+Insert
+Remove
+Replace
+ 
+
+Example 1:
+
+Input: 
+s = "geek", t = "gesek"
+Output: 1
+Explanation: One operation is required 
+inserting 's' between two 'e's of str1.
+Example 2:
+
+Input : 
+s = "gfg", t = "gfg"
+Output: 
+0
+Explanation: Both strings are same
+*/
