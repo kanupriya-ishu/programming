2600dc90c30fd4756888382a34a8413cc910cc9e
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-11 12:29:52 +0530

    Update EditDistance.java

--- data structures/string/EditDistance.java
+++ data structures/string/EditDistance.java
@@ -22,3 +22,56 @@ Output:
 0
 Explanation: Both strings are same
 */
+
+import java.util.*;
+public class Test {
+	
+    public static void main(String args[]) 
+    {  
+    	String s1 = "sunday";
+    	String s2 = "saturday";
+    	
+    	System.out.println(editDist(s1, s2, s1.length(), s2.length()));
+    }
+    
+    static int min(int x, int y, int z)
+    {
+        if (x <= y && x <= z)
+            return x;
+        if (y <= x && y <= z)
+            return y;
+        else
+            return z;
+    }
+
+    static int editDist(String str1, String str2, int m,
+                        int n)
+    {
+        // If first string is empty insert all characters of second string into first
+        if (m == 0)
+            return n;
+
+        // If second string is empty remove all characters of first string
+        if (n == 0)
+            return m;
+
+        // If last characters of two strings are same,
+        // nothing much to do. Ignore last characters and
+        // get count for remaining strings.
+        if (str1.charAt(m - 1) == str2.charAt(n - 1))
+            return editDist(str1, str2, m - 1, n - 1);
+
+        // If last characters are not same, consider all
+        // three operations on last character of first
+        // string, recursively compute minimum cost for all
+        // three operations and take minimum of three
+        // values.
+        return 1
+            + min(editDist(str1, str2, m, n - 1), // Insert
+                  editDist(str1, str2, m - 1, n), // Remove
+                  editDist(str1, str2, m - 1,
+                           n - 1) // Replace
+              );
+    }
+
+}
