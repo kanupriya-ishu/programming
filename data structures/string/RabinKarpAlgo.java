5b4618805067915245272d826a813bd6ec08d8eb
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-12 14:28:57 +0530

    Update RabinKarpAlgo.java

--- data structures/string/RabinKarpAlgo.java
+++ data structures/string/RabinKarpAlgo.java
@@ -14,3 +14,57 @@ Output: Pattern found at index 0
         Pattern found at index 9
         Pattern found at index 12
 */
+
+import java.util.*;
+public class RabinKarpAlgo {
+	public static final int d = 256;
+	 public static void main(String args[]) 
+	    {  
+		 	String text = "GEEKS FOR GEEKS";
+		    String pattern = "GEEK";
+		         
+		       // A prime number
+		     int prime_no = 101; 
+		     
+		       // Function Call
+		     search(pattern, text, prime_no);
+	    	
+	    }
+
+	private static void search(String pattern, String text, int prime_no) {
+		int m = pattern.length();
+		int n = text.length();
+		int p = 0; // hash value of pattern
+		int t = 0; // hash value of text
+		int h = 1;
+		
+		for(int i=0; i<m-1; i++) {
+			h = (h*d)%prime_no;
+		}
+		
+		// calculate hash value of pattern and first window of text
+		for(int i=0; i<m; i++) {
+			p = (d*p + pattern.charAt(i))%prime_no;
+			t = (d*t + text.charAt(i))%prime_no;
+		}
+		int i,j;
+		for(i=0; i<=n-m; i++) {
+			if(p==t) {
+				for(j=0; j<m; j++) {
+					if(pattern.charAt(j)!=text.charAt(j+i))
+						break;
+				}
+				
+				if(j==m) {
+					System.out.println("Pattern found at "+i);
+				}
+			}
+			
+			if(i<n-m) {
+				t = (d*(t-text.charAt(i)*h) + text.charAt(i+m))%prime_no;
+				if(t<0)
+					t += prime_no;
+			}
+		}
+	}
+}
