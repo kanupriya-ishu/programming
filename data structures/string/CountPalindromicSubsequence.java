dd721bf4d856cccebe1e688fda0348553a0e410c
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-12 15:27:46 +0530

    Added question

---
+++ data structures/string/CountPalindromicSubsequence.java
@@ -0,0 +1,16 @@
+/*
+Question:
+Find how many palindromic subsequences (need not necessarily be distinct) can be formed in a given string. Note that the empty string is not considered as a palindrome. 
+Examples: 
+
+Input : str = "abcd"
+Output : 4
+Explanation :- palindromic  subsequence are : "a" ,"b", "c" ,"d" 
+
+Input : str = "aab"
+Output : 4
+Explanation :- palindromic subsequence are :"a", "a", "b", "aa"
+
+Input : str = "aaaa"
+Output : 15
+*/
