e949a8bc8f764bfed558b18d81e45936756cfff7
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-14 11:04:01 +0530

    Update KMP.java

--- data structures/string/KMP.java
+++ data structures/string/KMP.java
@@ -15,3 +15,74 @@ Output: Pattern found at index 0
         Pattern found at index 12
 */
 
+import java.util.*;
+public class KMP {
+	
+	public static void main(String args[])
+	{
+	    String text = "AABAACAADAABAABA";
+	    String pattern = "AABA";
+	    KMPSearch(pattern, text);
+	}
+
+	private static void KMPSearch(String pattern, String text) {
+		int m = pattern.length();
+		int n = text.length();
+		
+		int lps[] = new int[m];
+		int j = 0; //index for pattern
+		
+		computeLPSarray(pattern, m, lps);
+		
+		int i=0; // index for text
+		while(i<n) {
+			
+			if(pattern.charAt(j)==text.charAt(i)) { // increment i and j till a mismatch is found
+				j++;
+				i++;
+			}
+			
+			if(j==m) {
+				System.out.println("Pattern found at "+(i-j));
+				j = lps[j-1];
+			}
+			
+			// mismatch after j matches
+			else if(i<n && pattern.charAt(j)!=text.charAt(i)) {
+				if(j!=0) {
+					j = lps[j-1];
+				}
+				
+				else {
+					i++;
+				}
+			}
+		}
+	}
+
+	private static void computeLPSarray(String pattern, int m, int[] lps) {
+		int len = 0; // length of previous longest prefix suffix
+		int i =1;
+		
+		lps[0] = 0;
+		
+		while(i<m) {
+			if(pattern.charAt(i)==pattern.charAt(len)) {
+				len++;
+				lps[i] = len;
+				i++;
+			}
+			else {
+				if(len!=0) {
+					len = lps[len-1];
+				}
+				
+				else {
+					lps[i] = len;
+					i++;
+				}
+			}
+		}
+	}
+
+}
