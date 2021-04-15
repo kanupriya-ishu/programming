7805c1e4c8a4eee00c50dbd0ab485d825533e199
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-15 20:59:04 +0530

    Added question

---
+++ data structures/string/LongestPrefixSuffix.java
@@ -0,0 +1,17 @@
+/*
+Problem:
+Given a string of characters, find the length of the longest proper prefix which is also a proper suffix.
+
+Example 1:
+
+Input: s = "abab"
+Output: 2
+Explanation: "ab" is the longest proper 
+prefix and suffix. 
+Example 2:
+
+Input: s = "aaaa"
+Output: 3
+Explanation: "aaa" is the longest proper 
+prefix and suffix. 
+*/
