464e3c092e2b55dc51d7be6818fe891b159ea232
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-16 13:02:24 +0530

    Update CountMinReversal.java

--- data structures/arrays/CountMinReversal.java
+++ data structures/arrays/CountMinReversal.java
@@ -21,3 +21,59 @@ Output: 1
 Input:  exp = "}{{}}{{{"
 Output: 3
 */
+
+import java.util.*;
+public class CountMinReversal {
+	
+	public static void main(String args[])
+	{
+		 String s = "}}{{";
+		 System.out.println(countMinReversal(s));
+	}
+
+	private static int countMinReversal(String s) {
+		
+		int n = s.length();
+		
+		if(n%2!=0) {
+			return -1;
+		}
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i=0; i<n; i++) {
+			char ch = s.charAt(i);
+			
+			if(ch=='}' && !stack.isEmpty()) {
+				if(stack.peek()!='{') {
+					stack.push(ch);
+				}
+				
+				else{
+					stack.pop();
+				}
+			}
+			
+			else {
+				stack.push(ch);
+			}
+		}
+		
+		int len = stack.size();
+		
+		int left = 0, right = 0;
+		while(!stack.isEmpty()) {
+			if(stack.peek()=='{') {
+				left++;
+				stack.pop();
+			}
+			
+			else {
+				right++;
+				stack.pop();
+			}
+		}
+		
+		return (int)(Math.ceil((double)left/2)+Math.ceil((double)right/2)) ;
+	}
+}
