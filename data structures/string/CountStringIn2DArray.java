9111521d93590170ac2371897a20fd71f223ab3f
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-17 12:15:37 +0530

    Added question

---
+++ data structures/string/CountStringIn2DArray.java
@@ -0,0 +1,29 @@
+/*
+Question:
+Given a 2-Dimensional character array and a string, we need to find the given string in 2-dimensional character array such that individual characters can be present left to right, right to left, top to down or down to top.
+
+Examples: 
+
+Input : a ={
+            {D,D,D,G,D,D},
+            {B,B,D,E,B,S},
+            {B,S,K,E,B,K},
+            {D,D,D,D,D,E},
+            {D,D,D,D,D,E},
+            {D,D,D,D,D,G}
+           }
+        str= "GEEKS"
+Output :2
+
+Input : a = {
+            {B,B,M,B,B,B},
+            {C,B,A,B,B,B},
+            {I,B,G,B,B,B},
+            {G,B,I,B,B,B},
+            {A,B,C,B,B,B},
+            {M,C,I,G,A,M}
+            }
+        str= "MAGIC"
+
+Output :4
+*/
