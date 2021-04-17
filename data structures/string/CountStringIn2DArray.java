5a6f8133af93d581641bd7361dee921c176b62da
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-17 12:16:04 +0530

    Update CountStringIn2DArray.java

--- data structures/string/CountStringIn2DArray.java
+++ data structures/string/CountStringIn2DArray.java
@@ -27,3 +27,50 @@ Input : a = {
 
 Output :4
 */
+
+import java.util.*;
+public class Test {
+	
+	public static void main(String args[])
+	{
+		 String s = "GEEKS";
+		 char arr[][] = {{'D','D','D','G','D','D'},
+				 		 {'B','B','D','E','B','S'},
+		 				 {'B','S','K','E','B','K'},
+		 				 {'D','D','D','D','D','E'},
+		 				 {'D','D','D','D','D','E'},
+		 				 {'D','D','D','D','D','G'}};
+		 
+		 int ans = 0;
+		 int size = s.length();
+		 
+		 for(int i=0; i<arr.length; i++) {
+			 for(int j=0; j<arr[0].length; j++) {
+				 ans += solve(i, j, s, arr, size, 0);
+			 }
+		 }
+		 
+		 System.out.println(ans);
+	}
+
+	private static int solve(int i, int j, String s, char[][] arr, int size, int k) {
+		int found = 0;
+		if(i>=0 && j>=0 && i<arr.length && j<arr[0].length && s.charAt(k)==arr[i][j]) {
+			char temp = s.charAt(k);
+			arr[i][j] = 0;
+			k++;
+			if(k==size) {
+				found = 1;
+			}
+			else {
+				found += solve(i+1, j, s, arr, size, k);
+				found += solve(i-1, j, s, arr, size, k);
+				found += solve(i, j+1, s, arr, size, k);
+				found += solve(i, j-1, s, arr, size, k);
+			}
+			arr[i][j] = temp;
+		}
+		
+		return found;
+	}
+}
