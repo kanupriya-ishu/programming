1363d5d7f56bbbb849d8cf22d6a5fc36fd0e9ff1
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-20 21:55:46 +0530

    Update LongestCommonPrefix.java

--- data structures/string/LongestCommonPrefix.java
+++ data structures/string/LongestCommonPrefix.java
@@ -15,3 +15,28 @@ Input: strs = ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
 */
+
+import java.util.*;
+
+public class LongestCommonPrefix {
+
+	static int NO_OF_CHARS = 256;
+
+	public static void main(String args[]) {
+		String[] strs = { "flower","flow","flight" };
+
+		if (strs.length == 0)
+			System.out.println("NO");
+		else {
+			String pre = strs[0];
+			for (int i = 1; i < strs.length; i++) {
+				while (strs[i].indexOf(pre) != 0) {
+					pre = pre.substring(0, pre.length() - 1);
+
+				}
+
+			}
+		}
+
+	}
+}
