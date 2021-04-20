ff4187ce6e1201a66f2a3703029fd880f2e8cdcb
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-20 16:59:54 +0530

    Added question

---
+++ data structures/string/SearchStringIn2DArray.java
@@ -0,0 +1,26 @@
+/*
+Question:
+Input:  grid[][] = {"GEEKSFORGEEKS",
+                    "GEEKSQUIZGEEK",
+                    "IDEQAPRACTICE"};
+        word = "GEEKS"
+
+Output: pattern found at 0, 0
+        pattern found at 0, 8
+        pattern found at 1, 0
+Explanation: 'GEEKS' can be found as prefix of
+1st 2 rows and suffix of first row
+
+Input:  grid[][] = {"GEEKSFORGEEKS",
+                    "GEEKSQUIZGEEK",
+                    "IDEQAPRACTICE"};
+        word = "EEE"
+
+Output: pattern found at 0, 2
+        pattern found at 0, 10
+        pattern found at 2, 2
+        pattern found at 2, 12
+Explanation: EEE can be found in first row 
+twice at index 2 and index 10
+and in second row at 2 and 12
+*/
