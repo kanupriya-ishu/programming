8e0c40782d4ae328d807074e75d6098a24c6df23
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-20 18:27:44 +0530

    Update RomanNumber.java

--- data structures/string/RomanNumber.java
+++ data structures/string/RomanNumber.java
@@ -21,3 +21,67 @@ Input:
 s = III 
 Output: 3
 */
+
+import java.util.*;
+public class RomanNumber {
+	
+	static int NO_OF_CHARS = 256;
+	
+	// 8 directions
+	static int[] x = {0,0,-1,1,1,-1,1,-1};
+	static int[] y = {-1,1,0,0,1,-1,-1,1};
+	
+	public static void main(String args[])
+	{
+		  String str = "MCMIV";
+		  int sum = 0;
+		  
+		  for(int i=0; i<str.length(); i++) {
+			int s1 = value(str.charAt(i));
+			
+			if(i+1 < str.length()) {
+				int s2 = value(str.charAt(i+1));
+				 if(s1>=s2) {
+					 sum += s1;
+				 }
+				 
+				 else {
+					 sum += s2 - s1;
+					 i++;
+				 }
+			}
+			
+			else {
+				sum += s1;
+			}
+		  }
+		  
+		  System.out.println(sum); 
+	}
+
+	private static int value(char ch) {
+		switch(ch) {
+			case 'I':
+				return 1;
+			
+			case 'V':
+				return 5;
+			
+			case 'X':
+				return 10;
+				
+			case 'L':
+				return 50;
+				
+			case 'C':
+				return 100;
+				
+			case 'D':
+				return 500;
+				
+			case 'M':
+				return 1000;
+		}
+		return -1;
+	}
+}
