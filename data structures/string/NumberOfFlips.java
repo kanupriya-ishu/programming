d3da5f3f1e88777520a7a40201a8139c4dc20057
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-20 22:06:23 +0530

    Added question

---
+++ data structures/string/NumberOfFlips.java
@@ -0,0 +1,20 @@
+/*
+Question:
+Given a binary string, that is it contains only 0s and 1s. We need to make this string a sequence of alternate characters by flipping some of the bits, our goal is to minimize the number of bits to be flipped.
+
+Example 1:
+
+Input:
+S = "001"
+Output: 1
+Explanation: We can flip the 0th bit to 1
+to have "101".
+
+Example 2:
+
+Input: 
+S = "0001010111"
+Output: 2
+Explanation: We can flip the 1st and 8th bit
+to have "0101010101".
+*/
