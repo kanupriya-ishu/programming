da461ebf30f6a3fcee4f3520a25a2f842d573513
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-20 18:27:17 +0530

    Added question

---
+++ data structures/string/RomanNumber.java
@@ -0,0 +1,23 @@
+/*
+Question:
+Given a string in roman no format (s)  your task is to convert it to an integer . Various symbols and their values are given below.
+I 1
+V 5
+X 10
+L 50
+C 100
+D 500
+M 1000
+
+Example 1:
+
+Input:
+s = V
+Output: 5
+
+Example 2:
+
+Input:
+s = III 
+Output: 3
+*/
