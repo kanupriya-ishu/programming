b843d2ac322f9aa2542d41cffa3debeedbc62c36
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-21 16:27:13 +0530

    Added question

---
+++ data structures/string/SecondMostRepeatedWord.java
@@ -0,0 +1,24 @@
+/*
+Question:
+Given a sequence of strings, the task is to find out the second most repeated (or frequent) string in the given sequence.
+
+Note: No two strings are the second most repeated, there will be always a single string.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {aaa, bbb, ccc, bbb, aaa, aaa}
+Output: bbb
+Explanation: "bbb" is the second most 
+occurring string with frequency 2.
+
+Example 2:
+
+Input: 
+N = 6
+arr[] = {geek, for, geek, for, geek, aaa}
+Output: for
+Explanation: "for" is the second most
+occurring string with frequency 2.
+*/
