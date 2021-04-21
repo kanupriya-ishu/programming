6ab64174c9a18f0e6ec201b6695715e5b899addc
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-21 19:10:28 +0530

    Update LongestCommonSubsequence.java

--- data structures/string/LongestCommonSubsequence.java
+++ data structures/string/LongestCommonSubsequence.java
@@ -23,3 +23,35 @@ Output: 2
 Explanation: LCS of "ABC" and "AC" is
 "AC" of length 2.
 */
+
+import java.util.*;
+
+public class LongestCommonSubsequence {
+
+	public static void main(String args[])
+	{
+		String s1 = "AGGTAB";
+	    String s2 = "GXTXAYB";
+	    
+	    char[] arr1 = s1.toCharArray();
+	    char[] arr2 = s2.toCharArray();
+	    
+	    int n1 = arr1.length;
+	    int n2 = arr2.length;
+	    
+	    System.out.println("Max length: " + lcs(arr1, arr2, n1, n2));
+	}
+
+	private static int lcs(char[] arr1, char[] arr2, int n1, int n2) {
+		if(n1==0 || n2==0)
+			return 0;
+		
+		if(arr1[n1-1] == arr2[n2-1])
+			return 1 + lcs(arr1, arr2, n1-1, n2-1);
+		
+		else
+			return Math.max(lcs(arr1, arr2, n1, n2-1), lcs(arr1, arr2, n1-1, n2));
+	}
+	
+	
+}
