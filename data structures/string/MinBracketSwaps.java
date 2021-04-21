46d6ddbd3f72073a1275a2b04c6e96f7c3fa04af
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-21 23:01:09 +0530

    Update MinBracketSwaps.java

--- data structures/string/MinBracketSwaps.java
+++ data structures/string/MinBracketSwaps.java
@@ -15,3 +15,57 @@ Input  : [[][]]
 Output : 0
 The string is already balanced.
 */
+
+import java.util.*;
+
+public class MinBracketSwaps {
+
+	public static void main(String args[])
+	{
+		String s = "[]][][";
+	    System.out.println(swapCount(s));
+	  
+	    s = "[[][]]";
+	    System.out.println(swapCount(s));
+	}
+
+	private static int swapCount(String s) {
+		List<Integer> arr = new ArrayList<>();
+		
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)=='[') {
+				arr.add(i);
+			}
+		}
+		
+		int sum = 0;
+		int p = 0;
+		int count = 0;
+		
+		char[] ch = s.toCharArray();
+		
+		for(int i=0; i<ch.length; i++) {
+			if(ch[i]=='[') {
+				count++;
+				p++;
+			}
+			
+			else if(ch[i]==']') {
+				count--;
+			}
+			
+			if(count<0) {
+				sum += arr.get(p) - i;
+				char temp = ch[i];
+				ch[i] = ch[arr.get(p)];
+				ch[arr.get(p)] = temp;
+				p++;
+				
+				count = 1;
+			}
+		}
+		return sum;
+	}
+
+
+}
