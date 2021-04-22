30d333417b1bb0f81b83a3838eb66e953cc9079c
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-22 23:12:52 +0530

    Update MinCharsToAddToMakeStringPalindrome.java

--- data structures/string/MinCharsToAddToMakeStringPalindrome.java
+++ data structures/string/MinCharsToAddToMakeStringPalindrome.java
@@ -14,3 +14,55 @@ Output : 2
 We can make above string palindrome as AAAACECAAAA
 by adding two A's at front of string.
 */
+
+import java.util.*;
+
+public class MinCharsToAddToMakeStringPalindrome {
+	
+	static final int MAX_CHARS =  256;
+
+	public static void main(String args[])
+	{
+		String s = "AACECAAAA";
+		System.out.println(getMin(s));	
+	}
+	
+	public static int getMin(String s) {
+		StringBuilder str = new StringBuilder();
+		
+		str.append(s);
+		
+		String rev = str.reverse().toString();
+		str.reverse().append("$").append(rev);
+		
+		int[] lps = new int[str.length()];
+		computeLPSarray(str.toString(), str.length(), lps);
+		
+		return s.length()-lps[str.length()-1];
+	}
+
+	private static void computeLPSarray(String pattern, int m, int[] lps) {
+		int len = 0; // length of previous longest prefix suffix
+		int i =1;
+		
+		lps[0] = 0;
+		
+		while(i<m) {
+			if(pattern.charAt(i)==pattern.charAt(len)) {
+				len++;
+				lps[i] = len;
+				i++;
+			}
+			else {
+				if(len!=0) {
+					len = lps[len-1];
+				}
+				
+				else {
+					lps[i] = len;
+					i++;
+				}
+			}
+		}
+	}
+}
