c860acd12e613ecef33ed3a4803ac2636140cc86
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-22 22:29:44 +0530

    Added question

---
+++ data structures/string/ReorganizeString.java
@@ -0,0 +1,20 @@
+/*
+Question:
+Given a string with repeated characters, the task is to rearrange characters in a string so that no two adjacent characters are same.
+
+Note : It may be assumed that the string has only lowercase English alphabets.
+
+Examples:
+
+Input: aaabc 
+Output: abaca 
+
+Input: aaabb
+Output: ababa 
+
+Input: aa 
+Output: Not Possible
+
+Input: aaaabc 
+Output: Not Possible
+*/
