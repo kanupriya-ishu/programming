5fdd9f97638c591e53e73d4674abbae314e958a6
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-22 23:12:17 +0530

    Added Question

---
+++ data structures/string/MinCharsToAddToMakeStringPalindrome.java
@@ -0,0 +1,16 @@
+/*
+Question:
+Given a string str we need to tell minimum characters to be added at front of string to make string palindrome.
+Examples: 
+ 
+
+Input  : str = "ABC"
+Output : 2
+We can make above string palindrome as "CBABC"
+by adding 'B' and 'C' at front.
+
+Input  : str = "AACECAAAA";
+Output : 2
+We can make above string palindrome as AAAACECAAAA
+by adding two A's at front of string.
+*/
