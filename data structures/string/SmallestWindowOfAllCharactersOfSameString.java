07882b926c16101ed8ca035c34e93361e5681ba1
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-22 22:10:33 +0530

    Added question

---
+++ data structures/string/SmallestWindowOfAllCharactersOfSameString.java
@@ -0,0 +1,23 @@
+/*
+Question:
+Given a string 's'. The task is to find the smallest window length that contains all the characters of the given string at least one time.
+For eg. A = “aabcbcdbca”, then the result would be 4 as of the smallest window will be “dbca”.
+
+ 
+
+Example 1:
+
+Input : "AABBBCBBAC"
+Output : 3
+Explanation : Sub-string -> "BAC"
+
+Example 2:
+Input : "aaab"
+Output : 2
+Explanation : Sub-string -> "ab"
+ 
+Example 3:
+Input : "GEEKSGEEKSFOR"
+Output : 8
+Explanation : Sub-string -> "GEEKSFOR"
+*/
