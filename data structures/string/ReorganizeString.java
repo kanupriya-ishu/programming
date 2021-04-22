1b76afd24a4ddb9a99d238d771df6d597ad21644
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-22 22:30:24 +0530

    Update ReorganizeString.java

--- data structures/string/ReorganizeString.java
+++ data structures/string/ReorganizeString.java
@@ -18,3 +18,53 @@ Output: Not Possible
 Input: aaaabc 
 Output: Not Possible
 */
+
+import java.util.*;
+
+public class ReorganizeString {
+	
+	static final int MAX_CHARS =  256;
+
+	public static void main(String args[])
+	{
+		String s = "aaabc";
+		System.out.println(reorganizeString(s));	
+	}
+
+	private static String reorganizeString(String s) {
+		Map<Character,Integer> counts = new HashMap<>();
+		for(char c: s.toCharArray()) {
+			counts.put(c, counts.getOrDefault(c, 0)+1);
+		}
+		
+		PriorityQueue<Character> maxHeap = new PriorityQueue<>((a,b) -> counts.get(b)-counts.get(a));
+		maxHeap.addAll(counts.keySet());
+		
+		StringBuilder result = new StringBuilder();
+		while(maxHeap.size() > 1) {
+			char current = maxHeap.remove();
+			char next = maxHeap.remove();
+			result.append(current);
+			result.append(next);
+			counts.put(current, counts.get(current)-1);
+			counts.put(next, counts.get(next)-1);
+			if(counts.get(current)>0) {
+				maxHeap.add(current);
+			}
+			
+			if(counts.get(next)>0) {
+				maxHeap.add(next);
+			}
+		}
+		
+		if (!maxHeap.isEmpty()) {
+			char last = maxHeap.remove();
+			if (counts.get(last)>1) {
+				return "";
+			}
+			result.append(last);
+		}
+		
+		return result.toString();
+	}
+}
