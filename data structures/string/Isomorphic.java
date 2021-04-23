b39813711469d4f1fcc5eb1415b3b6e44d163abb
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-23 19:10:58 +0530

    Update Isomorphic.java

--- data structures/string/Isomorphic.java
+++ data structures/string/Isomorphic.java
@@ -26,3 +26,50 @@ different charactersin xyz. So there
 won't be one to one mapping between
 str1 and str2.
 */
+
+import java.util.*;
+
+public class Isomorphic {
+	
+	private static int MAX_CHARS = 256;
+
+	public static void main(String args[])
+	{
+		String s1 = "aab";
+        String s2 = "xxy";
+        
+        System.out.println(isIsomorphic(s1, s2));
+	}
+
+	private static boolean isIsomorphic(String s1, String s2) {
+		if(s1.length() != s2.length()) {
+			return false;
+		}
+		
+		HashMap<Character, Character> map1 = new HashMap<>();
+		HashMap<Character, Boolean> map2 = new HashMap<>();
+		
+		for(int i=0; i<s1.length(); i++) {
+			char c1 = s1.charAt(i);
+			char c2 = s2.charAt(i);
+			
+			if(map1.containsKey(c1)) {
+				if(map1.get(c1)!=c2) {
+					return false;
+				}
+			}
+			
+			else {
+				if(map2.containsKey(c2)) {
+					return false;
+				}
+				else {
+					map1.put(c1, c2);
+					map2.put(c2, false);
+				}
+			}
+		}
+		
+		return true;
+	}
+}
