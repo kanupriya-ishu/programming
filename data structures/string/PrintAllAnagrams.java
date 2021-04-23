9bfdf5ba3937adacec8c131b181211b32c36a018
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-23 12:31:31 +0530

    Update PrintAllAnagrams.java

--- data structures/string/PrintAllAnagrams.java
+++ data structures/string/PrintAllAnagrams.java
@@ -29,3 +29,36 @@ There are 2 groups of
 anagrams "no", "on" make group 1.
 "is" makes group 2. 
 */
+ 
+import java.util.*;
+
+public class PrintAllAnagrams {
+
+	public static void main(String args[])
+	{
+		String[] strs = {"act", "god", "cat", "dog", "tac"};
+		
+		List<List<String>> groupedAnagrams = groupAnagrams(strs);
+		
+		System.out.println(groupedAnagrams);
+	}
+
+	private static List<List<String>> groupAnagrams(String[] strs) {
+		List<List<String>> groupedAnagrams = new ArrayList<>();
+		HashMap<String, List<String>> map = new HashMap<>();
+		
+		for(String current: strs) {
+			char[] characters = current.toCharArray();
+			Arrays.sort(characters);
+			String sorted = new String(characters);
+			if(!map.containsKey(sorted)) {
+				map.put(sorted, new ArrayList<>());
+			}
+			
+			map.get(sorted).add(current);
+		}
+		
+		groupedAnagrams.addAll(map.values());		
+		return groupedAnagrams;
+	}
+}
