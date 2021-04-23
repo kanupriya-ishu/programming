460ffb4173649c3d930d3a1d8598c37df1b5a3b2
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-23 12:45:15 +0530

    Update RemoveAdjacentDuplicates.java

--- data structures/string/RemoveAdjacentDuplicates.java
+++ data structures/string/RemoveAdjacentDuplicates.java
@@ -22,3 +22,34 @@ appearing 2nd time consecutively.
 'a' at fifth position is appearing
 2nd time consecutively.
 */
+
+import java.util.*;
+
+public class RemoveAdjacentDuplicates {
+
+	public static void main(String args[])
+	{
+		String str = "aabaa";
+		System.out.println(removeConsecutiveChars(str));
+	}
+
+	private static String removeConsecutiveChars(String str) {
+		String ans = "";
+		Stack<Character> stack = new Stack<>();
+		
+		int n = str.length();
+		
+		stack.push(str.charAt(0));
+		for(int i=1; i<n; i++) {
+			if(stack.peek()!=str.charAt(i)) {
+				stack.push(str.charAt(i));
+			}
+		}
+		
+		while(!stack.isEmpty()) {
+			ans = stack.pop() + ans;
+		}
+		
+		return ans;
+	}
+}
