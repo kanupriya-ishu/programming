42545ecfccba4cec8735f04831c3168b5ac110ff
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-23 19:10:27 +0530

    Added Question

---
+++ data structures/string/Isomorphic.java
@@ -0,0 +1,28 @@
+/*
+Question:
+Given two strings 'str1' and 'str2', check if these two strings are isomorphic to each other.
+Two strings str1 and str2 are called isomorphic if there is a one to one mapping possible for every character of str1 to every character of str2 while preserving the order.
+Note: All occurrences of every character in ‘str1’ should map to the same character in ‘str2’
+
+Example 1:
+
+Input:
+str1 = aab
+str2 = xxy
+Output: 1
+Explanation: There are two different
+charactersin aab and xxy, i.e a and b
+with frequency 2and 1 respectively.
+
+Example 2:
+
+Input:
+str1 = aab
+str2 = xyz
+Output:
+Explanation: There are two different
+charactersin aab but there are three
+different charactersin xyz. So there
+won't be one to one mapping between
+str1 and str2.
+*/
