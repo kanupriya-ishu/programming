a2e2abfd7b8d890715d75df925efdc3b24e4a0a3
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-26 18:32:23 +0530

    Update PermutationCoeff.java

--- data structures/dynamic programming/PermutationCoeff.java
+++ data structures/dynamic programming/PermutationCoeff.java
@@ -10,3 +10,33 @@ P(10, 0) = 1
 P(10, 1) = 10
 */
 
+import java.util.*;
+
+public class PermutationCoeff {
+
+	public static void main(String args[])
+	{
+		int n = 10;
+		int r = 2;
+		
+		System.out.println(P(n, r));
+	}
+
+	private static int P(int n, int r) {
+		int[][] P = new int[n+1][r+1];
+		
+		for(int i=0; i<=n; i++) {
+			for(int j=0; j<=Math.min(r, i); j++) {
+				if(j==0) {
+					P[i][j] = 1;
+				}
+				else {
+					P[i][j] = P[i-1][j] + j*P[i-1][j-1];
+				}
+			}
+		}
+		
+		return P[n][r];
+	}
+
+}
