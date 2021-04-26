e3edf52c17786a774ca38b86b4c4e9d632fa3cff
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-26 19:00:17 +0530

    Added question

---
+++ data structures/dynamic programming/Catalan.java
@@ -0,0 +1,6 @@
+/*
+Question:
+Catalan numbers are a sequence of natural numbers that occurs in many interesting counting problems.
+
+The first few Catalan numbers for n = 0, 1, 2, 3, … are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, …  
+*/
