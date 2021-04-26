4dcc1cdf5fea0df229980deb47957ade94aeea80
kanupriya-ishu <kanupriya2512@gmail.com>
2021-04-26 18:14:12 +0530

    Update Knapsack.java

--- data structures/dynamic programming/Knapsack.java
+++ data structures/dynamic programming/Knapsack.java
@@ -21,3 +21,35 @@ values[] = {1,2,3}
 weight[] = {4,5,6}
 Output: 0
 */
+
+import java.util.*;
+
+public class Knapsack {
+
+	public static void main(String args[])
+	{
+		int val[] = new int[] { 60, 100, 120 };
+        int wt[] = new int[] { 10, 20, 30 };
+        int W = 50;
+        int n = val.length;
+        System.out.println(knapSack(W, wt, val, n));
+	}
+
+	private static int knapSack(int W, int[] wt, int[] val, int n) {
+		int[][] arr = new int[n+1][W+1]; 
+		
+		for(int i=0; i<=n; i++) {
+			for(int w=0; w<=W; w++) {
+				if(i==0 || w==0) {
+					arr[i][w] = 0;
+				}
+				else if(wt[i-1]<=w) {
+					arr[i][w] = Math.max(val[i-1] + arr[i-1][w-wt[i-1]], arr[i-1][w]);
+				}
+				else {
+					arr[i][w] = arr[i-1][w];
+				}
+			}
+		}
+		return arr[n][W];
+	}
