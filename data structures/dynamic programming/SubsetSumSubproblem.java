dd0b9c2cb5c43e59d8448802081e5d70d6626e5f
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-13 15:50:18 +0530

    Update SubsetSumSubproblem.java

--- data structures/dynamic programming/SubsetSumSubproblem.java
+++ data structures/dynamic programming/SubsetSumSubproblem.java
@@ -36,12 +36,22 @@ public class SubsetSumSubproblem {
 	        sum += num;
 	    }
 	    
-	    if ((sum & 1) == 1) {
+	    if ((sum % 2) == 1) {
 	        return false;
 	    }
 	    sum /= 2;
 
 	    int n = nums.length;
+		
+		/*
+			Actually, this is a 0/1 knapsack problem, for each number, we can pick it or not. Let us assume dp[i][j] means whether the specific sum j can be gotten from the first i numbers. 
+			If we can pick such a series of numbers from 0-i whose sum is j, dp[i][j] is true, otherwise it is false.
+
+			Base case: dp[0][0] is true; (zero number consists of sum 0 is true)
+			Transition function: For each number, if we don't pick it, dp[i][j] = dp[i-1][j], which means if the first i-1 elements has made it to j, dp[i][j] would also make it to j (we can just ignore nums[i]). 
+			If we pick nums[i]. dp[i][j] = dp[i-1][j-nums[i]], which represents that j is composed of the current value nums[i] and the remaining composed of other previous numbers. 
+			Thus, the transition function is dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
+		*/
 	    boolean[][] dp = new boolean[n+1][sum+1];
 	    for (int i = 0; i < dp.length; i++) {
 	        Arrays.fill(dp[i], false);
@@ -60,7 +70,7 @@ public class SubsetSumSubproblem {
 	        for (int j = 1; j < sum+1; j++) {
 	            dp[i][j] = dp[i-1][j];
 	            if (j >= nums[i-1]) {
-	                dp[i][j] = (dp[i-1][j] || dp[i-1][j-nums[i-1]]);
+	                dp[i][j] = (dp[i][j] || dp[i-1][j-nums[i-1]]);
 	            }
 	        }
 	    }
