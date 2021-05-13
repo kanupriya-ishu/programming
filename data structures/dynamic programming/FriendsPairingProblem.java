e15123a10e80a4a251e5401e4ee3ec1f30aa2f86
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-13 17:21:14 +0530

    Update FriendsPairingProblem.java

--- data structures/dynamic programming/FriendsPairingProblem.java
+++ data structures/dynamic programming/FriendsPairingProblem.java
@@ -26,5 +26,37 @@ Explanation:
 Your Task:
 You don't need to read input or print anything. Your task is to complete the function countFriendsPairings() which accepts an integer n and return number of ways in which friends can remain single or can be paired up.
 
-
 */
+
+import java.util.*;
+
+public class FriendsPairingProblem {
+
+	public static void main(String args[])
+	{
+		int n = 5;
+		System.out.println(calculateWays(n));
+	}
+
+	private static int calculateWays(int n) {
+		if (n<=2) {
+			return n;
+		}
+		
+		int[] dp = new int[n+1];
+		
+		dp[1] = 1; // one number can be paired in only one way i.e. when it stays single
+		dp[2] = 2; // two numbers can be paired in two ways one: both numbers stay single, two: they pair up
+		
+		for(int i=3; i<=n; i++) {
+			// f(n) = f(n-1) + (n-1) * f(n-2)
+			// when you consider number from 1 to n, they can be paired in following way
+			// first: 1 stays single and n-1 numbers recurse = f(n-1)
+			// second: 1 pairs up with either of the other n-1 numbers, n-1 such pairs are possible: 1-2, 1-3...1-n
+			// and then n-2 numbers will remain and they will recurse = (n-1)*f(n-2)
+			dp[i] = dp[i-1] + (i-1)*dp[i-2];
+		}
+		
+		return dp[n];
+	}
+}
