49b7156d04b4a3142a568e63d2da167779968540
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-15 18:57:07 +0530

    Added Question

---
+++ data structures/dynamic programming/PaintingFence.java
@@ -0,0 +1,17 @@
+/*
+Problem:
+Given a fence with n posts and k colors, find out the number of ways of painting the fence such that at most 2 adjacent posts have the same color.
+
+Example 1:
+
+Input:
+N=3,  K=2 
+Output: 6
+
+Example 2:
+
+Input:
+N=2,  K=4
+Output: 16
+*/
+
