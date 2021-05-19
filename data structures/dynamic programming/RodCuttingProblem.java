42b8eccae192998db6b3095054fc3cac25d178aa
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-19 16:13:05 +0530

    Update RodCuttingProblem.java

--- data structures/dynamic programming/RodCuttingProblem.java
+++ data structures/dynamic programming/RodCuttingProblem.java
@@ -11,3 +11,47 @@ length   | 1   2   3   4   5   6   7   8
 --------------------------------------------
 price    | 3   5   8   9  10  17  17  20
 */
+
+import java.util.*;
+
+public class RodCuttingProblem {
+
+	public static void main(String args[])
+	{
+		int arr[] = new int[] {1, 5, 8, 9, 10, 17, 17, 20};
+        int size = arr.length;
+        System.out.println("Maximum Obtainable Value is "+
+                            cutRod(arr, size));
+	}
+
+	private static int cutRod(int[] arr, int size) {
+		int[] np = new int[size + 1];
+		np[0] = 0;
+		
+		for(int i=1; i<size+1; i++) {
+			np[i] = arr[i-1];
+		}
+		
+		int[] dp = new int[np.length];
+		
+		dp[0] = 0;
+		dp[1] = np[1];
+		
+		for(int i=2; i<dp.length; i++) {
+			dp[i] = np[i];
+			int left = 1;
+			int right = i-1;
+			
+			while(left<=right) {
+				if(dp[left] + dp[right] > dp[i]) {
+					dp[i] = dp[left] + dp[right];
+				}
+				left++;
+				right--;
+			}
+		}
+		
+		return dp[dp.length-1];
+	}
+
+}
