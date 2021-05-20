4767042a872ce88d1ae58c298617af8088edaa02
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-20 18:53:12 +0530

    Update LongestCommonSubsequence.java

--- data structures/dynamic programming/LongestCommonSubsequence.java
+++ data structures/dynamic programming/LongestCommonSubsequence.java
@@ -23,3 +23,48 @@ Output: 2
 Explanation: LCS of "ABC" and "AC" is
 "AC" of length 2.
 */
+
+import java.util.*;
+
+public class LongestCommonSubsequence {
+
+	public static void main(String args[])
+	{
+		String s1 = "ABCDGH";
+		String s2 = "AEDFHR";
+		
+		System.out.println(lcs(s1, s2));
+		
+	}
+
+	private static int lcs(String s1, String s2) {
+		int n1 = s1.length();
+		int n2 = s2.length();
+		
+		// dp[i][j] will hold longest subsequence of string s1 from index i to last index and string s2 from index j to last index
+		int[][] dp = new int[n1+1][n2+1];
+		
+		for(int i=n1; i>=0; i--) {
+			for(int j=n2; j>=0; j--) {
+				// last row and column represent empty character
+				if(i==n1 || j==n2) {
+					dp[i][j] = 0;
+				}
+				
+				else {
+					// if first character is same then the longest subsequence of string will be 1 + lcs((substring of s1 excluding first character) and (substring of s2 excluding first character))
+					if(s1.charAt(i)==s2.charAt(j)) {
+						dp[i][j] = 1 + dp[i+1][j+1];
+					}
+					// otherwise longest subsequence of string will be max of (lcs((string s1)and(string s2 excluding first character)), lcs((string s2)and(string s1 excluding first character)))
+					else {
+						dp[i][j] = Math.max(dp[i][j+1], dp[i+1][j]);
+					}
+				}
+			}
+		}
+		
+		return dp[0][0]; 
+	}
+
+}
