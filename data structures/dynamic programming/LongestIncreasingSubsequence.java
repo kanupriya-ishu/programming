03071a43215a42134471e9f1bda682c1327d45a7
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-20 23:45:43 +0530

    Update LongestIncreasingSubsequence.java

--- data structures/dynamic programming/LongestIncreasingSubsequence.java
+++ data structures/dynamic programming/LongestIncreasingSubsequence.java
@@ -23,3 +23,43 @@ Output: 3
 Explanation:Longest increasing subsequence
 5 7 9, with length 3
 */
+
+import java.util.*;
+
+public class LongestIncreasingSubsequence {
+
+	public static void main(String args[])
+	{
+		int n = 16;
+		
+		int[] arr = {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15};
+		
+		System.out.println(lis(arr, n));
+	}
+
+	private static int lis(int[] arr, int n) {
+		
+		int[] dp = new int[n];
+		
+		int max_lis = 0;
+		
+		for(int i=0; i<n; i++) {
+			int max = 0;
+			for(int j=0; j<i; j++) {
+				if(arr[j] < arr[i]) {
+					if(dp[j]>max) {
+						max = dp[j];
+					}
+				}
+			}
+			
+			dp[i] = max + 1;
+			if(dp[i]>max_lis) {
+				max_lis = dp[i];
+			}
+		}
+		
+		return max_lis;
+	}
+
+}
