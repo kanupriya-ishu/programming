7393cd946858bff7b8f3107d8b18103950d0a05e
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-20 23:02:56 +0530

    Added question

---
+++ data structures/dynamic programming/LongestRepeatedSubsequence.java
@@ -0,0 +1,17 @@
+/*
+Question:
+Given a string str, find length of the longest repeating subseequence such that the two subsequence don’t have same string character at same position, i.e., any i’th character in the two subsequences shouldn’t have the same index in the original string.
+ 
+
+Exampel 1:
+
+Input: str = "axxxy"
+Output: 2
+Explanation: The longest repeating subsequenece is "xx".
+
+Example 2:
+
+Input: str = "aab"
+output: 1
+Explanation: The longest reapting subsequenece is "a".
+*/
