9f92476e630077a48917b91029eb1aa674610dbd
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-21 15:55:38 +0530

    Added question

---
+++ data structures/dynamic programming/LCSof3Strings.java
@@ -0,0 +1,21 @@
+/*
+Question:
+Given 3 strings A, B and C, the task is to find the longest common sub-sequence in all three given sequences.
+
+Example 1:
+
+Input:
+A = "geeks", B = "geeksfor", 
+C = "geeksforgeeks"
+Output: 5
+Explanation: "geeks"is the longest common
+subsequence with length 5.
+
+Example 2:
+
+Input: 
+A = "abcd", B = "efgh", C = "ijkl"
+Output: 0
+Explanation: There's no common subsequence
+in all the strings.
+*/
