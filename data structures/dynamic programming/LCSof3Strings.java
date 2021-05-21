d61a9a71937a3c7d534f992bb005c741afd5b194
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-21 15:56:22 +0530

    Update LCSof3Strings.java

--- data structures/dynamic programming/LCSof3Strings.java
+++ data structures/dynamic programming/LCSof3Strings.java
@@ -19,3 +19,50 @@ Output: 0
 Explanation: There's no common subsequence
 in all the strings.
 */
+
+import java.util.*;
+
+public class LCSof3Strings {
+
+	public static void main(String args[])
+	{
+		String s1 = "AGGT12";
+        String s2 = "12TXAYB";
+        String s3 = "12XBA";
+		
+		System.out.println(lcs(s1, s2, s3));
+	}
+
+	private static int lcs(String s1, String s2, String s3) {
+		// TODO Auto-generated method stub
+		int n1 = s1.length();
+		int n2 = s2.length();
+		int n3 = s3.length();
+		
+		int[][][] dp = new int[n1+1][n2+1][n3+1];
+		
+		for(int i=n1; i>=0; i--) {
+			for(int j=n2; j>=0; j--) {
+				for(int k=n3; k>=0; k--) {
+					if (i==n1 || j==n2 || k==n3) {
+						dp[i][j][k] = 0;
+					}
+					
+					else {
+						if(s1.charAt(i)==s2.charAt(j) && s2.charAt(j)==s3.charAt(k)) {
+							dp[i][j][k] = 1 + dp[i+1][j+1][k+1];
+						}
+						
+						else {
+							dp[i][j][k] = Math.max(dp[i+1][j][k], Math.max(dp[i][j+1][k], dp[i][j][k+1]));
+						}
+					}
+				}
+			}
+		}
+		
+		return dp[0][0][0];
+	}
+
+
+}
