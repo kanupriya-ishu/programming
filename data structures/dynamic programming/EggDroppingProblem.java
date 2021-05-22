30059d4e6b2578c9b50ccccb2d8d2175513ca8a3
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-23 00:24:15 +0530

    Added question

---
+++ data structures/dynamic programming/EggDroppingProblem.java
@@ -0,0 +1,23 @@
+/*
+Question:
+
+Suppose you have N eggs and you want to determine from which floor in a K-floor building you can drop an egg such that it doesn't break. You have to determine the minimum number of attempts you need in order find the critical floor in the worst case while using the best strategy.There are few rules given below. 
+
+An egg that survives a fall can be used again.
+A broken egg must be discarded.
+The effect of a fall is the same for all eggs.
+If the egg doesn't break at a certain floor, it will not break at any floor below.
+If the eggs breaks at a certain floor, it will break at any floor above.
+
+Example 1:
+
+Input:
+N = 2, K = 10
+Output: 4
+
+Example 2:
+
+Input:
+N = 3, K = 5
+Output: 3
+*/
