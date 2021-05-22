c85865d368afcaf0166fd80bde2f730dca0aff82
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-22 19:03:38 +0530

    Update MaximumSubsequenceSumNo3Consecutive.java

--- data structures/dynamic programming/MaximumSubsequenceSumNo3Consecutive.java
+++ data structures/dynamic programming/MaximumSubsequenceSumNo3Consecutive.java
@@ -22,3 +22,43 @@ Output: 4
 Input: arr[] = {1, 2, 3, 4, 5, 6, 7, 8}
 Output: 27
 */
+
+import java.util.*;
+
+public class MaximumSubsequenceSumNo3Consecutive {
+
+	public static void main(String args[])
+	{
+		int arr[] = { 100, 1000, 100, 1000, 1 };
+        int n = arr.length;
+        System.out.println(maxSum(arr, n));
+	}
+
+	private static int maxSum(int[] arr, int n) {
+		
+		int[] sum = new int[n];
+		
+		if(n>0) {
+			sum[0] = arr[0];
+		}
+		
+		if(n>1) {
+			sum[1] = arr[0] + arr[1];
+		}
+		
+		if(n>2) {
+			sum[2] = Math.max(arr[1] + arr[2], Math.max(arr[0]+arr[2], sum[1]));
+		}
+		
+		for(int i=3; i<n; i++) {
+			// sum[i] will be max of 
+			// 1) excluding arr[i-2] => arr[i]+arr[i-1]+sum[i-3]
+			// 2) excluding arr[i-1] => (arr[i]+sum[i-2]
+			// 3) excluding arr[i] => sum[i-1]
+			sum[i] = Math.max(arr[i]+arr[i-1]+sum[i-3], Math.max(arr[i]+sum[i-2], sum[i-1]));
+		}
+		
+		return sum[n-1];
+	}
+
+}
