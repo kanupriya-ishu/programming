1fe2979235ca36d01f6f6a3bff3a6feb82438583
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-22 13:12:37 +0530

    Added Question

---
+++ data structures/dynamic programming/LongestSubsequencedifferenceBetweenAdjacentIsOne.java
@@ -0,0 +1,22 @@
+/*
+Question:
+Given an array A[] of size N, find the longest subsequence such that difference between adjacent elements is one.
+
+
+Example 1:
+
+Input: N = 7
+A[] = {10, 9, 4, 5, 4, 8, 6}
+Output: 3
+Explaination: The three possible subsequences 
+{10, 9, 8} , {4, 5, 4} and {4, 5, 6}.
+
+Example 2:
+
+Input: N = 5
+A[] = {1, 2, 3, 4, 5}
+Output: 5
+Explaination: All the elements can be 
+included in the subsequence.
+
+*/
