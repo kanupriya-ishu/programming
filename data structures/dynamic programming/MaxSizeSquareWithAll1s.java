bd6625ce4aecd5294f7f68c5d5a0fd44c463bbf0
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-23 15:58:29 +0530

    Added Question

---
+++ data structures/dynamic programming/MaxSizeSquareWithAll1s.java
@@ -0,0 +1,22 @@
+/*
+Question:
+Given a binary matrix mat of size n * m, find out the maximum size square sub-matrix with all 1s.
+
+Example 1:
+
+Input: n = 2, m = 2
+mat = {{1, 1}, 
+       {1, 1}}
+Output: 2
+Explaination: The maximum size of the square
+sub-matrix is 2. The matrix itself is the 
+maximum sized sub-matrix in this case.
+
+Example 2:
+
+Input: n = 2, m = 2
+mat = {{0, 0}, 
+       {0, 0}}
+Output: 0
+Explaination: There is no 1 in the matrix.
+*/
