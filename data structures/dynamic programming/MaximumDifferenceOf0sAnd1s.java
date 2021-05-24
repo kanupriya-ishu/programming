3a08793836ba6becd81e53169ef8173cadaebd9c
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-24 17:41:06 +0530

    Update MaximumDifferenceOf0sAnd1s.java

--- data structures/dynamic programming/MaximumDifferenceOf0sAnd1s.java
+++ data structures/dynamic programming/MaximumDifferenceOf0sAnd1s.java
@@ -18,3 +18,49 @@ Input: S = "111111"
 Output: -1
 Explanation: S contains 1s only 
 */
+
+import java.util.*;
+
+public class MaximumDifferenceOf0sAnd1s {
+
+	public static void main(String args[])
+	{
+		 String s = "11000010001";
+		 int n = s.length();
+		 System.out.println(maxDifference(s, n));
+	}
+
+	private static int maxDifference(String s, int n) {
+		
+		int[] dp = new int[s.length()];
+		
+		// fill the dp array
+		// if value at string is 0, put 1 in dp
+		// if value at string is 1, put -1 in dp
+		for(int i=0; i<n; i++) {
+			if(s.charAt(i)=='0') {
+				dp[i] = 1;
+			}
+			else {
+				dp[i] = -1;
+			}
+		}
+		
+		// apply Kadane's algorithm on the dp array
+		// the maximum contiguous sum is the answer 
+		int max_sum = 0;
+		int max_till_now = 0;
+		
+		for(int i=0; i<n; i++) {
+			max_till_now  = Math.max(max_till_now  + dp[i], dp[i]);
+			max_sum = Math.max(max_sum, max_till_now);
+		}
+		
+		if(max_sum==0){
+		    return -1;
+		}
+		
+		return max_sum;
+	}
+
+}
