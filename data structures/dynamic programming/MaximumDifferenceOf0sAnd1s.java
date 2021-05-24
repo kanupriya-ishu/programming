bc5917cc44032f6135177e543ba67cdf323b9d57
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-24 17:35:49 +0530

    Added question

---
+++ data structures/dynamic programming/MaximumDifferenceOf0sAnd1s.java
@@ -0,0 +1,20 @@
+/*
+Question:
+Given a binary string of 0s and 1s. The task is to find the maximum difference of the number of 0s and the number of 1s (number of 0s – number of 1s) in the substrings of a string.
+
+Note: In the case of all 1s, the answer will be -1.
+
+Example 1:
+
+Input : S = "11000010001" 
+Output : 6 
+Explanatio: From index 2 to index 9, 
+there are 7 0s and 1 1s, so number 
+of 0s - number of 1s is 6. 
+
+Example 2:
+
+Input: S = "111111"
+Output: -1
+Explanation: S contains 1s only 
+*/
