30e602c640522da1ed44863e84ec55e864ec4e03
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-25 23:22:47 +0530

    Update MinJumps.java

--- data structures/dynamic programming/MinJumps.java
+++ data structures/dynamic programming/MinJumps.java
@@ -24,3 +24,41 @@ Explanation:
 First we jump from the 1st to 2nd element 
 and then jump to the last element.
 */
+
+import java.util.*;
+
+public class MinJumps {
+
+	public static void main(String args[])
+	{
+		 int[] arr = {2,0,0,1,0,5} ;
+		 int n = arr.length;
+		 
+		 System.out.println(minJumps(arr, n));
+	}
+
+	private static int minJumps(int[] arr, int n) {
+		
+		int[] dp = new int[n];
+
+		dp[n-1] = 0;
+		
+		for(int i=n-2; i>=0; i--) {
+			int min = Integer.MAX_VALUE;
+			for(int j=1; j<=arr[i] && i+j<n; j++) {
+				
+				if(dp[i+j]<min && dp[i+j]!=-1) {
+					min = dp[i+j];
+				}
+			}
+			
+			if(min!=Integer.MAX_VALUE)
+				dp[i] = min + 1;
+			else {
+				dp[i] = -1;
+			}
+		}
+			
+		return dp[0];
+	}
+}
