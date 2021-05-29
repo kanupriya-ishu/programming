110f6dcad9484dec04ed8a544a662ff140f5ccc8
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-29 17:51:23 +0530

    Update LargestSumContiguousSubarray.java

--- data structures/dynamic programming/LargestSumContiguousSubarray.java
+++ data structures/dynamic programming/LargestSumContiguousSubarray.java
@@ -26,3 +26,41 @@ Explanation:
 Max subarray sum is -1 
 of element (-1)
 */
+
+import java.util.*;
+
+public class LargestSumContiguousSubarray {
+
+	public static void main(String args[])
+	{
+		int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3};
+        int n = arr.length;
+        maxSum(arr, n);
+	}
+
+	private static void maxSum(int[] arr, int n) {
+		
+		int i;
+		int sum=0, max_sum=0, s=0, start=0, end=0;
+		
+		for(i=0; i<n; i++) {
+			
+			if(arr[i]>sum+arr[i]) {
+				sum = arr[i];
+				s = i;
+			}
+			else {
+				sum += arr[i];
+			}
+			
+			if(sum>max_sum) {
+				max_sum = sum;
+				start = s;
+				end = i;
+			}
+		}
+		
+		System.out.println("Maximum sum: "+max_sum + "\nstart="+start+ "\nend="+end);
+	}
+
+}
