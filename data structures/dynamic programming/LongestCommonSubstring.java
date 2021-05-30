928d6b5579395204cd66acd6ebbf1f0038393d62
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-30 18:57:46 +0530

    Update LongestCommonSubstring.java

--- data structures/dynamic programming/LongestCommonSubstring.java
+++ data structures/dynamic programming/LongestCommonSubstring.java
@@ -17,3 +17,40 @@ Output: 1
 Explanation: The longest common substrings
 are "A", "B", "C" all having length 1.
 */
+
+import java.util.*;
+
+public class LongestCommonSubstring {
+
+	public static void main(String args[])
+	{
+		String s1 = "ABCDGH";
+		String s2 = "ACDGHR";
+		System.out.println(longestCommonSubstring(s1, s2));
+	}
+
+	private static int longestCommonSubstring(String s1, String s2) {
+		
+		int max = 0;
+		int n1 = s1.length();
+		int n2 = s2.length();
+		
+		int[][] dp = new int[n1+1][n2+1];
+		
+		for(int i=1; i<=n1; i++) {
+			for(int j=1; j<=n2; j++) {
+				if(s1.charAt(i-1)==s2.charAt(j-1)) {
+					dp[i][j] = 1 + dp[i-1][j-1];
+				}
+				else {
+					dp[i][j] = 0;
+				}
+				
+				max = Math.max(max, dp[i][j]);
+			}
+		}
+		
+		return max;
+	}
+
+}
