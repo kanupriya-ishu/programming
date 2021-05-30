242241a6883631982880426503537abeec660bee
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-30 22:14:33 +0530

    Update WordBreak.java

--- data structures/dynamic programming/WordBreak.java
+++ data structures/dynamic programming/WordBreak.java
@@ -25,3 +25,51 @@ Explanation: The string can be segmented as
 "i like samsung" or "i like sam sung".
  
 */
+
+import java.util.*;
+
+public class WordBreak {
+
+	public static void main(String args[])
+	{
+		int n = 12;
+		String[] dict = {"i", "like", "sam", "sung", "samsung", "mobile",
+				"ice","cream", "icecream", "man", "go", "mango"};
+		
+		String sentence = "ilike";
+		
+		
+		
+		System.out.println(wordBreak(dict, n, sentence));
+	}
+
+	private static int wordBreak(String[] dict, int n, String sentence) {
+		
+		List<String> dictionary = new ArrayList<>();
+		
+		for(int i=0; i<n; i++) {
+			dictionary.add(dict[i]);
+		}
+		
+		// dp[i] stores the number of sentences that can be made from 
+		// sentence starting at 0 index to ith index
+		int[] dp = new int[sentence.length()];
+		
+		for(int i=0; i<sentence.length(); i++) {
+			for(int j=0; j<=i; j++) {
+				String word = sentence.substring(j, i+1);
+				if(dictionary.contains(word)) {
+					
+					if(j>0) {
+						dp[i] += dp[j-1];
+					}
+					else {
+						dp[i] += 1;
+					}
+				}
+			}
+		}
+		return dp[sentence.length()-1]>0 ? 1 : 0;
+	}
+
+}
