8676443af1be4e7ee3150937199e05fcf6ee819d
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-30 16:28:54 +0530

    Update CountBalancedBinaryTree.java

--- data structures/dynamic programming/CountBalancedBinaryTree.java
+++ data structures/dynamic programming/CountBalancedBinaryTree.java
@@ -17,3 +17,44 @@ Output: 15
 Explanation: The maximum number of balanced
 binary trees possible with height 3 is 15. 
 */
+
+import java.util.*;
+
+public class CountBalancedBinaryTree {
+
+	public static void main(String args[])
+	{
+		int h = 3;
+		System.out.println(countBBT(h));
+	}
+
+	private static long countBBT(int h) {
+		
+		
+		int mod = 1000000007;
+		
+		long[] dp = new long[h+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		/*
+		Since the difference between the heights of left and right subtree is not more than one, possible heights of left and right part can be one of the following: 
+	 		(h-1), (h-2)
+			(h-2), (h-1)
+			(h-1), (h-1)
+			
+		count(h) = count(h-1) * count(h-2) + 
+			       count(h-2) * count(h-1) + 
+			       count(h-1) * count(h-1)
+			     = 2 * count(h-1) * count(h-2) + count(h-1) * count(h-1)
+			     = count(h-1) * (2*count(h - 2) + count(h - 1))
+		*/
+		
+		for(int i=2; i<=h; i++) {
+			dp[i] = ((dp[i-1]%mod) * (2*dp[i-2]%mod+dp[i-1]%mod))%mod ;
+		}
+		
+		return dp[h];
+	}
+
+}
