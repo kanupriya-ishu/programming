13a576df91582d84a61d7aea50c1fc3d55e3d3a8
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-30 18:57:11 +0530

    Added Question

---
+++ data structures/dynamic programming/LongestCommonSubstring.java
@@ -0,0 +1,19 @@
+/*
+Question:
+Given two strings. The task is to find the length of the longest common substring.
+
+
+Example 1:
+
+Input: S1 = "ABCDGH", S2 = "ACDGHR"
+Output: 4
+Explanation: The longest common substring
+is "CDGH" which has length 4.
+
+Example 2:
+
+Input: S1 = "ABC", S2 "ACB"
+Output: 1
+Explanation: The longest common substrings
+are "A", "B", "C" all having length 1.
+*/
