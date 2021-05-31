9abe387ec1eef4f7a457a8e310d0cffd76715d52
kanupriya-ishu <kanupriya2512@gmail.com>
2021-05-31 22:07:22 +0530

    Added Question

---
+++ data structures/dynamic programming/LongestPalindromicSubsequence.java
@@ -0,0 +1,20 @@
+/*
+Reference: https://www.youtube.com/watch?v=RiNzHfoA2Lo&t=16s
+
+Question:
+Given a sequence, find the length of the longest palindromic subsequence in it.
+
+Example 1:
+
+Input: 
+Str = "abcebc"
+Output: 
+4
+
+Example 2:
+
+Input: 
+Str = "abcd"
+Output: 
+1
+*/
