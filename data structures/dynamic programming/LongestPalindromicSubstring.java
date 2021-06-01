7883bb527c6cfb88b73b870005de010c896c7ecd
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-01 12:12:47 +0530

    Added Question

---
+++ data structures/dynamic programming/LongestPalindromicSubstring.java
@@ -0,0 +1,14 @@
+/*
+Reference: https://www.youtube.com/watch?v=WpYHNHofwjc
+
+Question:
+Given a string, find the longest substring which is palindrome. 
+
+For example, 
+
+Input: Given string :"forgeeksskeegfor", 
+Output: "geeksskeeg"
+
+Input: Given string :"Geeks", 
+Output: "ee"
+*/
