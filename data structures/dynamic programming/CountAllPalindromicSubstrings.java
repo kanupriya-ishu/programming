f8da8128da6e8db06b3c2df088c7b641c7a38d87
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-01 11:55:50 +0530

    Added Question

---
+++ data structures/dynamic programming/CountAllPalindromicSubstrings.java
@@ -0,0 +1,20 @@
+/*
+Reference: https://www.youtube.com/watch?v=XmSOWnL6T_I
+
+Question:
+Given a string, the task is to count all palindrome sub string in a given string. Length of palindrome sub string is greater than or equal to 2. 
+
+Examples: 
+
+Input : str = "abaab"
+Output: 3
+Explanation : 
+All palindrome substring are :
+ "aba" , "aa" , "baab" 
+
+Input : str = "abbaeae"
+Output: 4
+Explanation : 
+All palindrome substring are : 
+"bb" , "abba" ,"aea","eae"
+*/
