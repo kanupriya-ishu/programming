f048eb4c1c02315a7e8c92498de73d557d7b2bef
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-01 12:34:12 +0530

    Added Question

---
+++ data structures/dynamic programming/LISTree.java
@@ -0,0 +1,4 @@
+/*
+Question:
+Given a Binary Tree, find size of the Largest Independent Set(LIS) in it. A subset of all tree nodes is an independent set if there is no edge between any two nodes of the subset. 
+*/
