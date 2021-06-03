0df54645cf9b52c5a15f069fe84cd33c0dc65063
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-03 18:10:16 +0530

    Added Question

---
+++ data structures/dynamic programming/CoinGameWinner.java
@@ -0,0 +1,17 @@
+/*
+Question:
+A and B are playing a game. At the beginning there are n coins. Given two more numbers x and y. In each move a player can pick x or y or 1 coins. A always starts the game. The player who picks the last coin wins the game or the person who is not able to pick any coin loses the game. For a given value of n, find whether A will win the game or not if both are playing optimally.
+
+Examples: 
+
+Input :  n = 5, x = 3, y = 4
+Output : A
+There are 5 coins, every player can pick 1 or
+3 or 4 coins on his/her turn.
+A can win by picking 3 coins in first chance.
+Now 2 coins will be left so B will pick one 
+coin and now A can win by picking the last coin.
+
+Input : 2 3 4
+Output : B
+*/
