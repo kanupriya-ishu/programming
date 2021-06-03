954f69879fec2fd1b81a23fc6012edb8f21fb723
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-03 18:10:43 +0530

    Update CoinGameWinner.java

--- data structures/dynamic programming/CoinGameWinner.java
+++ data structures/dynamic programming/CoinGameWinner.java
@@ -15,3 +15,52 @@ coin and now A can win by picking the last coin.
 Input : 2 3 4
 Output : B
 */
+
+import java.util.*;
+
+public class CoinGameWinner {
+
+    public static void main(String[] args)
+    {     
+        int n = 5;
+        int x = 3;
+        int y = 4;
+        
+        System.out.println(winner(n, x, y));
+    }
+
+	private static char winner(int n, int x, int y) {
+		
+		boolean[] dp = new boolean[n+1];
+		
+		dp[0] = false; // There are no coins and A can not pick any coins thus A loses
+		dp[1] = true; // A will play the first move 
+		
+		for(int i=2; i<=n; i++) {
+			// if previous move is false i.e. if A did not play the last move
+			// then only A can play
+			// then we have to check if A can play for 1 coin
+			// if not we will check if A can play for x coins
+			// if not we will check if A can play for y coins
+			if (i - 1 >= 0 && dp[i - 1] == false)
+                dp[i] = true;
+            else if (i - x >= 0 && dp[i - x] == false)
+                dp[i] = true;
+            else if (i - y >= 0 && dp[i - y] == false)
+                dp[i] = true;
+			
+			else {
+				dp[i] = false;
+			}
+		}
+		
+		// if last move is played by A i.e. it is true then A wins
+		if(dp[n]) {
+			return 'A';
+		}
+		
+		// Otherwise B wins
+		return 'B';
+	}
+	 
+}
