a72204671cca111ac0e9c0c138cd415cda6e17dd
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-11 18:52:38 +0530

    Added question

---
+++ data structures/dynamic programming/LongestAlternatingSubsequence.java
@@ -0,0 +1,21 @@
+/*
+Question:
+A sequence {x1, x2, .. xn} is alternating sequence if its elements satisfy one of the following relations :
+x1 < x2 > x3 < x4 > x5..... or  x1 >x2 < x3 > x4 < x5.....
+Your task is to find the longest such sequence.
+
+Example 1:
+
+Input: nums = {1,5,4}
+Output: 3
+Explanation: The entire sequenece is a 
+alternating sequence.
+
+Example 2:
+
+Input: nums = {1,17,5,10,13,15,10,5,16,8}
+Ooutput: 7
+Explanation: There are several subsequences
+that achieve this length. 
+One is {1,17,10,13,10,16,8}.
+*/
