bef36e6d03ceef3c98b3e4d003d50e06bd40289b
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-16 19:09:18 +0530

    Added Question

---
+++ data structures/dynamic programming/OptimalBST.java
@@ -0,0 +1,5 @@
+/*
+Question:
+Given a sorted array key [0.. n-1] of search keys and an array freq[0.. n-1] of frequency counts, where freq[i] is the number of searches for keys[i]. 
+Construct a binary search tree of all keys such that the total cost of all the searches is as small as possible.
+*/
