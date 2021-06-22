b68889e10bc6888e3694694a18acd7b76462c949
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-22 15:12:40 +0530

    Added Question

---
+++ data structures/dynamic programming/PalindromePartitioningProblem.java
@@ -0,0 +1,20 @@
+/*
+Reference: https://www.youtube.com/watch?v=qmTtAbOTqcg
+
+Question:
+Given a string str, a partitioning of the string is a palindrome partitioning if every sub-string of the partition is a palindrome. Determine the fewest cuts needed for palindrome partitioning of given string.
+
+
+Example 1:
+
+Input: str = "ababbbabbababa"
+Output: 3
+Explaination: After 3 partitioning substrings 
+are "a", "babbbab", "b", "ababa".
+Example 2:
+
+Input: str = "aaabba"
+Output: 1
+Explaination: The substrings after 1
+partitioning are "aa" and "abba".
+*/
