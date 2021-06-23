452c3a95d1adb4da289647498e562503aa345a68
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-23 16:37:17 +0530

    Added Question

---
+++ data structures/dynamic programming/BooleanParenthesizationProblem.java
@@ -0,0 +1,33 @@
+/*
+Question:
+
+Given a boolean expression S of length N with following symbols.
+Symbols
+    'T' ---> true
+    'F' ---> false
+and following operators filled between symbols
+Operators
+    &   ---> boolean AND
+    |   ---> boolean OR
+    ^   ---> boolean XOR
+Count the number of ways we can parenthesize the expression so that the value of expression evaluates to true.
+
+ 
+
+Example 1:
+
+Input: N = 7
+S = T|T&F^T
+Output: 4
+Explaination: The expression evaluates 
+to true in 4 ways ((T|T)&(F^T)), 
+(T|(T&(F^T))), (((T|T)&F)^T) and (T|((T&F)^T)).
+
+Example 2:
+
+Input: N = 5
+S = T^F|F
+Output: 2
+Explaination: ((T^F)|F) and (T^(F|F)) are the 
+only ways.
+*/
