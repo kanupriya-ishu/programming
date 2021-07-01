9c0209e0509fa0efd0e76362644806ac2c708e0a
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-01 13:03:58 +0530

    Added Question

---
+++ data structures/dynamic programming/MaximumLengthOfPairChain.java
@@ -0,0 +1,25 @@
+/*
+Question:
+You are given N pairs of numbers. In every pair, the first number is always smaller than the second number. A pair (c, d) can follow another pair (a, b) if b < c. Chain of pairs can be formed in this fashion. You have to find the longest chain which can be formed from the given set of pairs. 
+ 
+
+Example 1:
+
+Input:
+N = 5
+P[] = {5  24 , 39 60 , 15 28 , 27 40 , 50 90}
+Output: 3
+Explanation: The given pairs are { {5, 24}, {39, 60},
+{15, 28}, {27, 40}, {50, 90} },the longest chain that
+can be formed is of length 3, and the chain is
+{{5, 24}, {27, 40}, {50, 90}}
+
+Example 2:
+
+Input:
+N = 2
+P[] = {5 10 , 1 11}
+Output: 1
+Explanation:The max length chain possible is only of
+length one.
+*/
