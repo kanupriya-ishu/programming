59fb858a1bf722adcb14f1036c76f8333e0f40df
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-01 13:04:34 +0530

    Update MaximumLengthOfPairChain.java

--- data structures/dynamic programming/MaximumLengthOfPairChain.java
+++ data structures/dynamic programming/MaximumLengthOfPairChain.java
@@ -23,3 +23,64 @@ Output: 1
 Explanation:The max length chain possible is only of
 length one.
 */
+
+import java.util.*;
+public class MaximumLengthOfPairChain {
+
+	public static void main(String[] args) {
+		Pair arr[] = new Pair[]{
+		  new Pair(5,24),
+		  new Pair(15, 25),                     
+		  new Pair (27, 40),
+		  new Pair(50, 60)};
+		System.out.println(maxChainLength(arr, arr.length));
+	}
+
+	private static int maxChainLength(Pair[] arr, int length) {
+		
+		// sort array based on first element
+		Arrays.sort(arr, new Comparator<Pair>() {
+            @Override public int compare(Pair p1, Pair p2)
+            {
+                return p1.a - p2.a;
+            }
+        });
+		
+		int n = arr.length;
+		int[] dp = new int[n];
+		int max = Integer.MIN_VALUE;
+		
+		// fill array with 1
+		Arrays.fill(dp, 1);
+		
+		for(int i=1; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				// if first element of arr[i] is greater than second element of arr[j]
+				// and length of pair chain of j => dp[j] + 1 is greater than current length dp[i]
+				// then store that value in dp[i]
+				if(arr[i].a > arr[j].b && dp[i]<dp[j]+1) {
+					dp[i] = dp[j]+1;
+				}
+				
+				// keep a track of max length of chain
+				max = Math.max(max, dp[i]);
+			}
+		}
+		
+		return max;
+	}
+}
+
+
+
+class Pair
+{
+    int a;
+    int b;
+     
+    public Pair(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
