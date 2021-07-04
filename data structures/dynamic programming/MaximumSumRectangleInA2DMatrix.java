5847b697553d089baad8f4dac86994f0d169dbf4
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-04 20:48:28 +0530

    Added Question

---
+++ data structures/dynamic programming/MaximumSumRectangleInA2DMatrix.java
@@ -0,0 +1,30 @@
+/*
+Reference: https://www.youtube.com/watch?v=-FgseNO-6Gk
+
+Question:
+Given a 2D matrix M of dimensions RxC. Find the maximum sum subarray in it.
+
+Example 1:
+
+Input:
+R=4
+C=5
+M=[[1,2,-1,-4,-20],
+[-8,-3,4,2,1],
+[3,8,10,1,3],
+[-4,-1,1,7,-6]]
+Output:
+29
+
+Example 2:
+
+Input:
+R=2
+C=2
+M=[[-1,-2],[-3,-4]]
+Output:
+-1
+Explanation:
+Taking only the first cell is the 
+optimal choice.
+*/
