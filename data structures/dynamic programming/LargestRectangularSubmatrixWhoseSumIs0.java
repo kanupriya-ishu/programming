9446d3207a2634f59e2ac28fc6551722ae36ec38
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-04 23:04:14 +0530

    Added Question

---
+++ data structures/dynamic programming/LargestRectangularSubmatrixWhoseSumIs0.java
@@ -0,0 +1,22 @@
+/*
+Question:
+Given a 2D matrix, find the largest rectangular sub-matrix whose sum is 0. 
+
+Examples:
+
+Input :  1,  2,  3
+        -3, -2, -1          
+         1,  7,  5
+
+Output : 1,  2,  3          
+        -3, -2, -1
+
+Input :  9,  7, 16,  5
+         1, -6, -7,  3          
+         1,  8,  7,  9          
+         7, -2,  0, 10
+
+Output :-6, -7
+          8,  7          
+         -2,  0 
+*/
