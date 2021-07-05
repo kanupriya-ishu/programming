c1dbcc8e601b6765bf58259fc95ca728ea7a9274
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-05 17:21:31 +0530

    Added Question

---
+++ data structures/dynamic programming/InterleavedStrings.java
@@ -0,0 +1,22 @@
+/*
+Reference: https://www.youtube.com/watch?v=jaQF6FSWYdE
+
+Question:
+Given three strings A, B and C your task is to complete the function isInterleave which returns true if C is an interleaving of A and B else returns false.
+String C is said to be interleaving A and B, if it contains all characters of A and B and order of all characters in individual strings is preserved.
+
+Example 1:
+
+Input:
+A = YX, B = X, C = XXY
+Output: 0
+Explanation: XXY is not interleaving
+of YX and X
+Example 2:
+
+Input:
+A = XY, B = X, C = XXY
+Output: 1
+Explanation: XXY is interleaving of
+XY and X.
+*/
