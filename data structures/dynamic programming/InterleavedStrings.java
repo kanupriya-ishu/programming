436277554f1f75c0442df8f4a3b75de530d73837
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-05 17:22:15 +0530

    Update InterleavedStrings.java

--- data structures/dynamic programming/InterleavedStrings.java
+++ data structures/dynamic programming/InterleavedStrings.java
@@ -20,3 +20,40 @@ Output: 1
 Explanation: XXY is interleaving of
 XY and X.
 */
+
+import java.util.*;
+public class InterleavedStrings {
+	
+	public static void main(String[] args) {
+		String A = "YX";
+		String B = "X";
+		String C = "XXY";
+		int n = A.length();
+		int m = B.length();
+		int len = C.length();
+		
+		int[][] dp = new int[n+1][m+1];
+		for(int[] sub: dp) {
+			Arrays.fill(sub, -1);
+		}
+		System.out.println(isInterleave(dp, A,B,C, n,m,len));
+	}
+
+	private static int isInterleave(int[][] dp, String A, String B, String C, int n, int m, int len) {
+		// TODO Auto-generated method stub
+		if(len==0) return 1;
+		if(dp[n][m]!=-1) return dp[n][m];
+		
+		int a=0, b=0;
+		if(n-1>=0 && A.charAt(n-1)==C.charAt(len-1)) {
+			a = isInterleave(dp, A, B, C, n-1, m, len-1);
+		}
+		if(m-1>=0 && B.charAt(m-1)==C.charAt(len-1)) {
+			b = isInterleave(dp, A, B, C, n, m-1, len-1);
+		}
+		
+		return dp[n][m] = a==1 || b==1 ? 1 : 0;
+		
+	}
+}
+
