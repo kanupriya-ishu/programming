79abcce05699f597a72e848c862b19f3f76bec7d
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-05 15:49:37 +0530

    Added Question

---
+++ data structures/dynamic programming/LargestAreaRectangleWithEqual1s0s.java
@@ -0,0 +1,18 @@
+/*
+Question:
+Given a binary matrix. The problem is to find the largest area rectangular sub-matrix with equal number of 1’s and 0’s.
+
+Examples:
+
+Input : mat[][] = { {0, 0, 1, 1},
+                    {0, 1, 1, 0},
+                    {1, 1, 1, 0},
+                    {1, 0, 0, 1} }
+Output : 8 sq. units
+(Top, left): (0, 0)
+(Bottom, right): (3, 1)
+
+Input : mat[][] = { {0, 0, 1, 1},
+                    {0, 1, 1, 1} }            
+Output : 6 sq. units
+*/
