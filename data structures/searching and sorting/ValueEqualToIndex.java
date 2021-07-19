6cb308d890b9b5c32d5cfe858a4788efd213ab50
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-19 13:38:17 +0530

    Added Question

---
+++ data structures/searching and sorting/ValueEqualToIndex.java
@@ -0,0 +1,20 @@
+/*
+Question:
+Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value.
+
+Example 1:
+
+Input: 
+N = 5
+Arr[] = {15, 2, 45, 12, 7}
+Output: 2
+Explanation: Only Arr[2] = 2 exists here.
+
+Example 2:
+
+Input: 
+N = 1
+Arr[] = {1}
+Output: 1
+Explanation: Here Arr[1] = 1 exists.
+*/
