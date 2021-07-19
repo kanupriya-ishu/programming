0140cf7021990c3aaedfa5351062cd6e3f2ffa51
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-19 13:25:49 +0530

    Update FirstAndLastElement.java

--- data structures/searching and sorting/FirstAndLastElement.java
+++ data structures/searching and sorting/FirstAndLastElement.java
@@ -19,3 +19,60 @@ arr[] = { 1, 3, 5, 5, 5, 5, 7, 123, 125 }
 Output:  6 6 
 
 */
+
+import java.util.*;
+public class FirstAndLastElement {
+	
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, 5, 5, 5, 5, 67, 123, 125 };
+		int n = arr.length;
+		int x = 5;
+		
+		System.out.println("First: " + first(arr,n,x));
+		System.out.println("Last: " + last(arr,n,x));
+	}
+
+	private static int first(int[] arr, int n, int x) {
+		int l = 0;
+		int r = n-1;
+		
+		int res = -1;
+		while(l<=r) {
+			int mid = l + (r-l)/2;
+			if(x>arr[mid]) {
+				 l = mid + 1;
+			}
+			else if(x<arr[mid]) {
+				r = mid-1;
+			}
+			else {
+				res = mid;
+				r = mid-1;
+			}
+		}
+		return res;
+	}
+
+	private static int last(int[] arr, int n, int x) {
+		int l = 0;
+		int r = n-1;
+		
+		int res = -1;
+		while(l<=r) {
+			int mid = l + (r-l)/2;
+			if(x>arr[mid]) {
+				 l = mid + 1;
+			}
+			else if(x<arr[mid]) {
+				r = mid-1;
+			}
+			else {
+				res = mid;
+				l = mid + 1;
+			}
+		}
+		return res;
+	}
+
+}
+
