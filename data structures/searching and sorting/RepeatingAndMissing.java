90e4c41894fa53a55cc9327931559a8cfd865912
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-29 11:36:57 +0530

    Added question

---
+++ data structures/searching and sorting/RepeatingAndMissing.java
@@ -0,0 +1,22 @@
+/*
+Question:
+Given an unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2, …N} is missing and one number 'B' occurs twice in array. Find these two numbers.
+
+Example 1:
+
+Input:
+N = 2
+Arr[] = {2, 2}
+Output: 2 1
+Explanation: Repeating number is 2 and 
+smallest positive missing number is 1.
+
+Example 2:
+
+Input:
+N = 3
+Arr[] = {1, 3, 3}
+Output: 3 2
+Explanation: Repeating number is 3 and 
+smallest positive missing number is 2.
+*/
