b55616e4db11dd2c9bc33a732df167725f5ed4dd
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-29 17:59:08 +0530

    Update FindPairGivenDifference.java

--- data structures/searching and sorting/FindPairGivenDifference.java
+++ data structures/searching and sorting/FindPairGivenDifference.java
@@ -18,3 +18,36 @@ arr[] = {90, 70, 20, 80, 50}
 Output: -1
 Explanation: There is no pair with difference of 45.
 */
+
+package solution;
+import java.util.*;
+public class FindPairGivenDifference {
+	
+	public static void main(String[] args) {
+		int[] arr = {90, 70, 20, 80, 50};
+		int n = arr.length;
+		int l = 78;
+		System.out.println(findPair(arr, n, l));
+	}
+
+	private static int findPair(int[] arr, int n, int l) {
+		Arrays.sort(arr);
+		
+		int i=0, j=1;
+		
+		while(i<n && j<n) {
+			if(i!=j && arr[j]-arr[i]==l) {
+				return 1;
+			}
+			else if(arr[j]-arr[i]<l) {
+				j++;
+			}
+			else {
+				i++;
+			}
+		}
+		return -1;
+	}
+
+}
+
