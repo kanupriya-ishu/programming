4e9550f5092eb1f8191564ce5729f7a9f8d36ba3
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-29 11:38:45 +0530

    Update RepeatingAndMissing.java

--- data structures/searching and sorting/RepeatingAndMissing.java
+++ data structures/searching and sorting/RepeatingAndMissing.java
@@ -20,3 +20,39 @@ Output: 3 2
 Explanation: Repeating number is 3 and 
 smallest positive missing number is 2.
 */
+
+package solution;
+import java.util.*;
+public class RepeatingAndMissing {
+	
+	public static void main(String[] args) {
+		int[] arr = {4, 2, 2, 5, 1};
+		int n = arr.length;
+		missingRepeated(arr, n);
+	}
+
+	private static void missingRepeated(int[] arr, int n) {
+		
+		for(int i=0; i<n; i++) {
+			if(arr[i]!=i+1) {
+				int temp = arr[arr[i]-1];
+				arr[arr[i]-1] = arr[i];
+				arr[i] = temp; 
+
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(arr[i]!=i+1) {
+				System.out.println("Repeated number = "+arr[i]);
+				System.out.println("Missing number = "+(i+1));
+				break;
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+
+}
+
