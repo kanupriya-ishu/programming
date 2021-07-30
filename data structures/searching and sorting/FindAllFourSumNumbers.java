e3bc9c6577675c32e4373ace938867c411c4a7f8
kanupriya-ishu <kanupriya2512@gmail.com>
2021-07-30 13:51:20 +0530

    Update FindAllFourSumNumbers.java

--- data structures/searching and sorting/FindAllFourSumNumbers.java
+++ data structures/searching and sorting/FindAllFourSumNumbers.java
@@ -24,3 +24,66 @@ sum of 2, 4, 7, 10 = 23 and sum of 3,
 5, 7, 8 = 23.
 
 */
+
+package solution;
+import java.util.*;
+public class FindAllFourSumNumbers {
+	
+	public static void main(String[] args) {
+		int[] arr = {10,2,3,4,5,7,8};
+		int n = arr.length;
+		int k = 23;
+		System.out.println(findAllFourNumbers(arr, n, k));
+	}
+
+	private static List<List<Integer>> findAllFourNumbers(int[] arr, int n, int k) {
+		Arrays.sort(arr);
+		ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=i+1; j<n; j++) {
+				int target = k - arr[j] - arr[i];
+				int left = j+1;
+				int right = n-1;
+				
+				while(left<right) {
+					int sum = arr[left] + arr[right];
+					
+					if(sum<target) left++;
+					
+					else if(sum>target) right--;
+					
+					else {
+						List<Integer> sub = new ArrayList<>();
+						sub.add(arr[i]);
+						sub.add(arr[j]);
+						sub.add(arr[left]);
+						sub.add(arr[right]);
+						res.add(sub);
+						
+						// processing the duplicates of number 3
+						while(left<right && sub.get(2)==arr[left]) {
+							left++;
+						}
+						
+						// processing the duplicates of number 4
+						while(left<right && sub.get(3)==arr[right]) {
+							right--;
+						}
+					}
+				}
+				
+				// processing the duplicates of number 2
+				while(j+1<n && arr[j+1]==arr[j]) {
+					j++;
+				}
+			}
+			
+			// processing the duplicates of number 1
+			while(i+1<n && arr[i+1]==arr[i]) {
+				i++;
+			}
+		}
+		return res;
+	}	
+}
