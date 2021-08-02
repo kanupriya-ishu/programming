c112c0febff96f67ec780ceffe62226a3ded4ff5
kanupriya-ishu <kanupriya2512@gmail.com>
2021-08-02 09:47:34 +0530

    Update CountTripletWithSumSmallerThanX.java

--- data structures/searching and sorting/CountTripletWithSumSmallerThanX.java
+++ data structures/searching and sorting/CountTripletWithSumSmallerThanX.java
@@ -22,3 +22,33 @@ Explanation: Below are triplets with
 sum less than 12 (1, 3, 4), (1, 3, 5), 
 (1, 3, 7) and (1, 4, 5).
 */
+
+package solution;
+import java.util.*;
+public class CountTripletWithSumSmallerThanX {
+	
+	public static void main(String[] args) {
+		int[] arr = {-2, 0, 1, 3};
+		int n = arr.length;
+		int x = 2;
+		System.out.println(countTriplets(arr, n, x));
+	}
+
+	private static int countTriplets(int[] arr, int n, int x) {
+		Arrays.sort(arr);
+		int count = 0;
+		for(int i=0; i<n-2; i++) {
+			int j =i+1, k = n-1;
+			while(j<k) {
+				if(arr[i] + arr[j] + arr[k] >= x) {
+					k--;
+				}
+				else {
+					count += k-j;
+					j++;
+				}
+			}
+		}
+		return count;
+	}
+}
