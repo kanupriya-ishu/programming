3196969a97b3f9869e8527a8fe6e1eb08f6c59d7
kanupriya-ishu <kanupriya2512@gmail.com>
2021-08-02 09:46:46 +0530

    Added question

---
+++ data structures/searching and sorting/CountTripletWithSumSmallerThanX.java
@@ -0,0 +1,24 @@
+/*
+Reference: https://www.youtube.com/watch?v=7nso5Dgc6CI
+
+Question:
+Given an array arr[] of distinct integers of size N and a sum value X, the task is to find count of triplets with the sum smaller than the given sum value X.
+
+
+Example 1:
+
+Input: N = 4, X = 2
+arr[] = {-2, 0, 1, 3}
+Output:  2
+Explanation: Below are triplets with 
+sum less than 2 (-2, 0, 1) and (-2, 0, 3). 
+
+Example 2:
+
+Input: N = 5, X = 12
+arr[] = {5, 1, 3, 4, 7}
+Output: 4
+Explanation: Below are triplets with 
+sum less than 12 (1, 3, 4), (1, 3, 5), 
+(1, 3, 7) and (1, 4, 5).
+*/
