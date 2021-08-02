4b1b95c4094949dba87d8638df0acd329e974b97
kanupriya-ishu <kanupriya2512@gmail.com>
2021-08-02 14:03:49 +0530

    Update ZeroSumSubarrays.java

--- data structures/searching and sorting/ZeroSumSubarrays.java
+++ data structures/searching and sorting/ZeroSumSubarrays.java
@@ -24,3 +24,45 @@ Explanation: The 4 subarrays are [-1 -3 4]
 [-2 2], [2 4 6 -12], and [-1 -3 4 -2 2]
 
 */
+
+package solution;
+import java.util.*;
+public class ZeroSumSubarrays {
+	
+	public static void main(String[] args) {
+		int[] arr = {6,-1,-3,4,-2,2,4,6,-12,-7};
+		int n = arr.length;
+		System.out.println(count(arr, n));
+	}
+
+	private static int count(int[] arr, int n) {
+		int count = 0;
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+		
+		int sum = 0;
+		
+		/*
+		 * Logic:
+		 * Keep on calculating sum
+		 * For each sum check if it exists in hashmap
+		 * if it does add its frequency to count
+		 * otherwise add the sum in the hashmap with frequency 1
+		 * */
+		for(int i=0; i<n; i++) {
+			sum += arr[i];
+			if(map.containsKey(sum)) {
+				int val = map.get(sum);
+				count+= val;
+				map.put(sum, val + 1);
+			}
+			else {
+				map.put(sum, 1);
+			}
+		}
+		
+		return count;
+	}
+	
+}
