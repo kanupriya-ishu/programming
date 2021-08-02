8892e05b50ec45c1465675937709e7502b95483f
kanupriya-ishu <kanupriya2512@gmail.com>
2021-08-02 13:04:25 +0530

    Added question

---
+++ data structures/searching and sorting/ZeroSumSubarrays.java
@@ -0,0 +1,26 @@
+/*
+Reference: https://www.youtube.com/watch?v=C9-n_H7dsvU
+
+Question:
+ou are given an array arr[] of size n. Find the total count of sub-arrays having their sum equal to 0.
+
+
+Example 1:
+
+Input:
+n = 6
+arr[] = {0,0,5,5,0,0}
+Output: 6
+Explanation: The 6 subarrays are 
+[0], [0], [0], [0], [0,0], and [0,0].
+
+Example 2:
+
+Input:
+n = 10
+arr[] = {6,-1,-3,4,-2,2,4,6,-12,-7}
+Output: 4
+Explanation: The 4 subarrays are [-1 -3 4]
+[-2 2], [2 4 6 -12], and [-1 -3 4 -2 2]
+
+*/
