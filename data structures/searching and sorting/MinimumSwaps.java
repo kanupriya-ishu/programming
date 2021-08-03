9f55f7260d10d28c23c892291c0b81261318f8ea
kanupriya-ishu <kanupriya2512@gmail.com>
2021-08-03 15:27:24 +0530

    Update MinimumSwaps.java

--- data structures/searching and sorting/MinimumSwaps.java
+++ data structures/searching and sorting/MinimumSwaps.java
@@ -21,3 +21,63 @@ Output:
 Explaination:
 swap 10 with 3 and swap 19 with 5.
 */
+
+package solution;
+import java.util.*;
+import java.util.stream.*;
+public class MinimumSwaps {
+	
+	public static void main(String[] args) {
+		int[] arr = {1,5,4,3,2};
+		int n = arr.length;
+		System.out.println(minSwaps(arr, n));
+	}
+
+	private static int minSwaps(int[] arr, int n) {
+		int count = 0;
+		
+		ArrayList<Pair> res = new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			res.add(new Pair(arr[i], i));
+		}
+		
+		res.sort(new Comparator<Pair>() {
+
+			@Override
+			public int compare(Pair o1, Pair o2) {
+				if(o1.first < o2.first)
+					return -1;
+				else if(o1.first == o2.first)
+					return 0;
+				return 1;
+			}
+		});
+		
+		for(int i=0; i<n; i++) {
+			if(res.get(i).second==i) {
+				continue;
+			}
+			else {
+				count++;
+				int temp1 = res.get(i).second;
+				int temp2 = res.get(res.get(i).second).second;
+				res.get(temp2).second = temp2;
+				res.get(temp1).second = temp1;
+				i--;
+			}
+		}
+
+		return count;
+	}
+
+}
+
+class Pair{
+	int first;
+	int second;
+	
+	Pair(int first, int second){
+		this.first = first;
+		this.second = second;
+	}
+}
